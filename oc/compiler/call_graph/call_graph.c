57d8ae6a23e654e9eb2a516a31f50d64ef104e0a
jackr276 <jack_m_robbins@me.com>
2025-01-27 13:31:48 -0500

    Add call graph module

---
+++ oc/compiler/call_graph/call_graph.c
@@ -0,0 +1,7 @@
+/**
+ * This is the implementation file for the call graph defined in call_graph.h
+*/
+
+#include "call_graph.h"
+
+
---
+++ oc/compiler/call_graph/call_graph.h
@@ -0,0 +1,15 @@
+/**
+ * This module represents the call-graph IR for the ollie language
+ *
+ * The call graph has a node for each procedure and an edge for each call site
+*/
+
+#ifndef CALL_GRAPH_H
+#define CALL_GRAPH_H
+
+//A call graph struct. Simply represents a function and edges to the functions that it calls
+typedef struct call_graph_node_t call_graph_node_t;
+
+
+
+#endif
