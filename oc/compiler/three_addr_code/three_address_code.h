cde6f6ee993ae531d6f3f159630bed88fcbae18c
jackr276 <jack_m_robbins@me.com>
2025-02-07 12:52:22 -0500

    Begin work on three address code module

--- oc/compiler/three_addr_code/three_address_code.h
+++ oc/compiler/three_addr_code/three_address_code.h
@@ -9,6 +9,44 @@
 #ifndef THREE_ADDRESS_CODE_H
 #define THREE_ADDRESS_CODE_H
 
+//For symtab linking
+#include "../symtab/symtab.h"
+#include <sys/types.h>
+
+//A struct that holds all knowledge of three address codes
+typedef struct three_addr_code_stmt three_addr_code_stmt;
+//A struct that holds our three address variables
+typedef struct three_addr_var three_addr_var;
+
+/**
+ * A three address var may be a temp variable or it may be
+ * linked to a non-temp variable. It keeps a generation counter
+ * for eventual SSA and type information
+*/
+struct three_addr_var{
+	//For convenience
+	char var_name[115];
+	//Link to symtab(NULL if not there)
+	symtab_variable_record_t* linked_var;
+	//Is this a temp variable?
+	u_int8_t is_temporary;
+	//Store the type info for faster access
+	//Types will be used for eventual register assignment
+	generic_type_t* type;
+};
+
+
+struct three_addr_code_stmt{
+	//A three address code always has 2 operands and an assignee
+	three_addr_var* op1;
+	three_addr_var* op2;
+	three_addr_var* assignee;
+	//TODO may add more
+};
+
+
+three_addr_code_stmt* 
+
 
 
 #endif
