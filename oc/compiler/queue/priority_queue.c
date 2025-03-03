0c6c19ffd3355ad6f7bb583f04b32363cc687c4a
jackr276 <jack_m_robbins@me.com>
2025-03-03 18:20:48 -0500

    Begin work on priority queue utility module

--- oc/compiler/queue/priority_queue.c
+++ oc/compiler/queue/priority_queue.c
@@ -5,3 +5,7 @@
 */
 
 #include "priority_queue.h"
+
+// Initially the queue size is 50. This is usually enough for most switch statements. Of course
+// if a user writes more than 50 cases, it will be accomodated
+#define INITIAL_QUEUE_SIZE 50;
--- oc/compiler/queue/priority_queue.h
+++ oc/compiler/queue/priority_queue.h
@@ -8,5 +8,59 @@
 #ifndef PRIORITY_QUEUE_H
 #define PRIORITY_QUEUE_H
 
+//Overall priority queue struct
+#include <sys/types.h>
+typedef struct priority_queue_t priority_queue_t;
+//The nodes in our priority queue
+typedef struct priority_queue_node_t priority_queue_node_t;
+
+/**
+ * Did we need to reorder, or was it already in order
+ */
+typedef enum{
+	OUT_OF_ORDER_INSERT,
+	IN_ORDER_INSERT,
+} insertion_status_t;
+
+/**
+ * This struct will usually be passed by copy -- it contains a pointer 
+ * to the internal heap
+*/
+struct priority_queue_t{
+	//The actual heap that exists in the priority queue
+	priority_queue_node_t* heap;
+	//The current size
+	u_int16_t next_index;
+	//The maximum size
+	u_int16_t maximum_size;
+};
+
+/**
+ * Each individual node is stored here. The priority needs to be stored 
+ * along with the pointer, which is what makes these nodes necessary
+*/
+struct priority_queue_node_t{
+	//Our priority
+	u_int16_t priority;
+	//What is actually in here - usually an AST node,
+	//but this could be anything if needed
+	void* node;
+};
+
+/**
+ * Insert a node with a given priority into the priority queue. Return
+ * an enum that dictates if we needed to reorder
+*/
+insertion_status_t priority_queue_enqueue(priority_queue_t* queue, void* ptr, u_int16_t priority);
+
+/**
+ * Dequeue from the priority queue
+*/
+void* priority_queue_dequeue(priority_queue_t* queue);
+
+/**
+ * Is the priority queue empty?
+*/
+u_int8_t priority_queue_is_empty(priority_queue_t* queue);
 
 #endif
