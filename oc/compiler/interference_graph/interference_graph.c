a9ea625d097f8814d775d1e98f59ee56231e99ca
jackr276 <jack_m_robbins@me.com>
2025-05-26 09:52:43 -0400

    #89: add new module for interference graph, tie into build system

---
+++ oc/compiler/interference_graph/interference_graph.c
@@ -0,0 +1,8 @@
+/**
+ * Author: Jack Robbins
+ *
+ * This file contains the implementations of the APIs
+ * defined in the interference graph header file
+*/
+
+
---
+++ oc/compiler/interference_graph/interference_graph.h
@@ -0,0 +1,15 @@
+/**
+ * Author: Jack Robbins
+ *
+ * This header file contains the APIs and data structures for the register allocator's interference graph
+*/
+
+//Header guards
+#ifndef INTERFERENCE_GRAPH_H
+#define INTERFERENCE_GRAPH_H
+
+
+
+#endif
+
+
