a00cf72674bfeb5f43a8fdcbef7de70dc6cf1034
jackr276 <jack_m_robbins@me.com>
2025-09-07 08:03:08 -0400

    #239: redo deletion and realignment functions

--- oc/compiler/stack_data_area/stack_data_area.c
+++ oc/compiler/stack_data_area/stack_data_area.c
@@ -7,7 +7,6 @@
 
 #include "stack_data_area.h"
 #include <stdio.h>
-#include <stdlib.h>
 #include <sys/types.h>
 #include "../instruction/instruction.h"
 
@@ -90,25 +89,6 @@ void add_variable_to_stack(stack_data_area_t* area, void* variable){
 	three_addr_var_t* var = variable;
 
 	/**
-	 * Special case: this is the very first thing that we've added onto 
-	 * the stack. If this is the case, we can add it, update the size,
-	 * and leave
-	 */
-	if(area->variables->current_index == 0){
-		//Add it on
-		dynamic_array_add(area->variables, var);
-
-		//Stack offset for this one is 0
-		var->stack_offset = 0;
-
-		//Add the overall size to the stack
-		area->total_size += var->type->type_size;
-
-		//And we're done so leave out
-		return;
-	}
-
-	/**
 	 * To align new variables that are added onto the stack, we will pad
 	 * their starting addresses as needed to ensure that the starting
 	 * address of the variable is a multiple of alignable type size
@@ -145,81 +125,64 @@ void add_variable_to_stack(stack_data_area_t* area, void* variable){
 
 
 /**
- * Remove a node from the stack if it is deemed useless
- *
- * We'll need to traverse the linked list and ultimately delete this
- * value from it. We shouldn't have to worry about realigning because everything
- * will already be aligned properly
+ * Completely realign every piece of data in the stack data
+ * area. This is only done after a deletion takes place
  */
-void remove_variable_from_stack(stack_data_area_t* area, void* variable){
-	//The node where we hopefully find this value
-	stack_data_area_node_t* current = area->highest;
+static void realign_data_area(stack_data_area_t* area){
+	//We're completely restarting here, so set this to 0
+	area->total_size = 0;
 
-	//Keep going until we either find it or run off of the list
-	while(current != NULL && current->variable != variable){
-		current = current->next;
-	}
+	//Run through every single variable
+	for(u_int16_t i = 0; i < area->variables->current_index; i++){
+		//Grab it out
+		three_addr_var_t* variable = dynamic_array_get_at(area->variables, i);
 
-	//This should never happen, but if it does, we'll need to notify the developer in an obvious way
-	if(current == NULL){
-		printf("[FATAL COMPILER ERROR]: Attempt to remove a nonexistent variable from stack_data_area\n");
-		exit(1);
-	}
+		/**
+		 * To align new variables that are added onto the stack, we will pad
+		 * their starting addresses as needed to ensure that the starting
+		 * address of the variable is a multiple of alignable type size
+		 */
 
-	//Otherwise we found it. Now to remove it, we'll need to sever the connections in an appropriate way
-	
-	//First remove the space
-	area->total_size -= current->variable_size;
+		//Get the type that we need to align by for the new var
+		generic_type_t* base_alignment = get_base_alignment_type(variable->type);
 
-	//Let's also update this variable's related offset to avoid confusion down the line
-	((three_addr_var_t*)(variable))->stack_offset = 0;
+		//Get the alignment size
+		u_int32_t alignable_size = base_alignment->type_size;
 
-	//Special case - it's the head
-	if(area->highest == current){
-		//Reserve this one's address
-		stack_data_area_node_t* temp = area->highest;
-		//Advance to the next one
-		area->highest = area->highest->next;
-		//Now we deallocate temp
-		free(temp);
+		//What will the starting address of the new variable be?
+		u_int32_t new_variable_offset = area->total_size;
 
-		//If this is the case we can recalculate
-		if(area->highest != NULL){
-			//Forget the reference to the dead node
-			area->highest->previous = NULL;
-			//Recalculate all of these offsets
-			recalculate_all_offsets(area->highest);
+		//How much padding do we need? Initially we assume none
+		u_int32_t needed_padding = 0;
+
+		//We can just use the overall data area size for this
+		if(area->total_size % alignable_size != 0){
+			//Grab the needed padding
+			needed_padding = area->total_size % alignable_size;
 		}
 
-	//Other special case - it's the tail
-	} else if(current->next == NULL){
-		//Just NULL this out and we're good
-		current->previous->next = NULL;
-
-		//This offset is now nothing - it's at the bottom
-		current->previous->offset = 0;
-
-		//Update this one's variable too
-		((three_addr_var_t*)(current->previous->variable))->stack_offset = 0;
-
-		//Now redo all of our offsets
-		recalculate_all_offsets(current->previous);
-
-		//And scrap current
-		free(current);
-
-	//Otherwise it's now the head
-	} else {
-		//This will cut out current completely
-		current->previous->next = current->next;
-		current->next->previous = current->previous;
-
-		//Use the next field to recalculate all offsets
-		recalculate_all_offsets(current->next);
-
-		//and now we scrap this one
-		free(current);
+		//This one's stack offset is the original total size plus whatever padding we need
+		variable->stack_offset = area->total_size + needed_padding;
+		
+		//Update the total size of the stack too. The new size is the original size
+		//with the needed padding and the new type's size added onto it
+		area->total_size = area->total_size + needed_padding + variable->type->type_size;
 	}
+}
+
+
+/**
+ * Remove a node from the stack if it is deemed useless
+ *
+ * Once we're done removing, we'll need to completely redo the data area's alignment
+ *
+ */
+void remove_variable_from_stack(stack_data_area_t* area, void* variable){
+	//Delete this variable
+	dynamic_array_delete(area->variables, variable);
+
+	//Realign the whole thing
+	realign_data_area(area);
 }
 
 
