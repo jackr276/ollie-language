d106e1f121b267ac0718bf00bdea817fa24140b9
jackr276 <jack_m_robbins@me.com>
2025-10-11 07:23:10 -0400

    Turn on used variables for function parameters
    Signed-off-by: jackr276 <jack_m_robbins@me.com>

--- oc/compiler/register_allocator/register_allocator.c
+++ oc/compiler/register_allocator/register_allocator.c
@@ -774,8 +774,8 @@ static void construct_function_call_live_ranges(dynamic_array_t* live_ranges, ba
 		//Extract it
 		three_addr_var_t* parameter = dynamic_array_get_at(function_parameters, i);
 
-		//NOT BROKEN, but disabled for now
-		//assign_live_range_to_source_variable(live_ranges, basic_block, parameter);
+		//This counts as a used live range
+		assign_live_range_to_source_variable(live_ranges, basic_block, parameter);
 	}
 }
 
