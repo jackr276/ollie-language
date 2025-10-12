63753e5fab218ec3cab10e899dbb332873fba7f8
jackr276 <jack_m_robbins@me.com>
2025-10-12 10:47:38 -0400

    Division instructions also cannot be combined

--- oc/compiler/instruction_selector/instruction_selector.c
+++ oc/compiler/instruction_selector/instruction_selector.c
@@ -2030,6 +2030,8 @@ static void handle_division_instruction(instruction_window_t* window){
 
 	//Once we've done all that, we need one final movement operation
 	instruction_t* result_movement = emit_movX_instruction(division_instruction->assignee, division->destination_register);
+	//This cannot be combined
+	result_movement->cannot_be_combined = TRUE;
 
 	//Insert this before the original division instruction
 	insert_instruction_before_given(result_movement, division_instruction);
@@ -2116,6 +2118,8 @@ static void handle_modulus_instruction(instruction_window_t* window){
 
 	//Once we've done all that, we need one final movement operation
 	instruction_t* result_movement = emit_movX_instruction(modulus_instruction->assignee, division->destination_register);
+	//This also cannot be combined
+	result_movement->cannot_be_combined = TRUE;
 
 	//Insert this after the original modulus
 	insert_instruction_after_given(result_movement, modulus_instruction);
