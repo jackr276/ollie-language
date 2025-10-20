cd43736dc52a407d1b637f7532a3dee8733b5873
jackr276 <jack_m_robbins@me.com>
2025-10-20 07:05:24 -0400

    #348: add constants.h header

---
+++ oc/compiler/utils/constants.h
@@ -0,0 +1,14 @@
+/**
+ * Author: Jack Robbins
+ * This file contains the definitions for all of the constants used
+ * by the ollie compiler
+*/
+
+//Include guards
+#ifndef OLLIE_COMPILER_CONSTANTS_H
+#define OLLIE_COMPILER_CONSTANTS_H
+
+#define TRUE 1
+#define FALSE 0
+
+#endif /* OLLIE_COMPILER_CONSTANTS_H */
