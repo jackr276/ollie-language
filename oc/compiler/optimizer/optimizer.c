90f663413c49582860def7a9549370acabf90fc1
jackr276 <jack_m_robbins@me.com>
2025-10-21 07:39:19 -0400

    Add test code emission for logical and branch, seems promising

--- oc/compiler/optimizer/optimizer.c
+++ oc/compiler/optimizer/optimizer.c
@@ -1544,6 +1544,24 @@ static void optimize_logical_and_branch_logic(instruction_t* short_circuit_statm
 	//And if the type is signed
 	u_int8_t first_half_signed = is_type_signed(first_half_cursor->assignee->type);
 
+	//The conditional decider is by default the assignee
+	three_addr_var_t* first_branch_conditional_decider = first_half_cursor->assignee;
+
+	//This is possible - if it happens we need to emit test code
+	if(first_half_cursor->op == BLANK){
+		//This is now the first half's conditional decider
+		first_branch_conditional_decider = emit_temp_var(first_half_cursor->assignee->type);
+
+		//Test instruction, we're just testing against ourselves here
+		instruction_t* test = emit_test_statement(first_branch_conditional_decider, first_half_cursor->assignee, first_half_cursor->assignee);
+
+		//Throw it into the block
+		add_statement(original_block, test);
+
+		//This now counts as a use
+		add_used_variable(original_block, first_half_cursor->assignee);
+	}
+
 	//Determine an appropriate branch. Remember, if this *fails* the if condition
 	//succeeds, so this is an *inverse* jump
 	branch_type_t first_half_branch = select_appropriate_branch_statement(first_condition_op, BRANCH_CATEGORY_INVERSE, first_half_signed);
@@ -1553,7 +1571,7 @@ static void optimize_logical_and_branch_logic(instruction_t* short_circuit_statm
 	//	goto else
 	//else
 	//	goto second_half_block 
-	emit_branch(original_block, else_target, second_half_block, first_half_branch, first_half_cursor->assignee, BRANCH_CATEGORY_NORMAL);
+	emit_branch(original_block, else_target, second_half_block, first_half_branch, first_branch_conditional_decider, BRANCH_CATEGORY_NORMAL);
 
 	/**
 	 * HANDLING THE SECOND BLOCK
@@ -1564,6 +1582,24 @@ static void optimize_logical_and_branch_logic(instruction_t* short_circuit_statm
 	ollie_token_t second_condition_op = second_half_cursor->op;
 	//And if the type is signed
 	u_int8_t second_half_signed = is_type_signed(second_half_cursor->assignee->type);
+
+	//The conditional decider is by default the assignee
+	three_addr_var_t* second_branch_conditional_decider = second_half_cursor->assignee;
+
+	//This is possible - if it happens we need to emit test code
+	if(second_half_cursor->op == BLANK){
+		//This is now the first half's conditional decider
+		second_branch_conditional_decider = emit_temp_var(second_half_cursor->assignee->type);
+
+		//Test instruction, we're just testing against ourselves here
+		instruction_t* test = emit_test_statement(second_branch_conditional_decider, second_half_cursor->assignee, second_half_cursor->assignee);
+
+		//Throw it into the block
+		add_statement(second_half_block, test);
+
+		//This now counts as a use
+		add_used_variable(original_block, second_half_cursor->assignee);
+	}
 
 	//Determine an appropriate branch. Remember, if this *succeeds* the if condition
 	//succeeds, so this is a *regular* jump
