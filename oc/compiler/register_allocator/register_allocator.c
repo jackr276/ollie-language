e02f3e0b36c96ff998f3728f68fbd73695c0d6ca
jackr276 <jack_m_robbins@me.com>
2025-11-09 08:48:59 -0500

    Some improvement noticed

--- oc/compiler/register_allocator/register_allocator.c
+++ oc/compiler/register_allocator/register_allocator.c
@@ -2731,8 +2731,18 @@ static instruction_t* insert_caller_saved_logic_for_direct_call(instruction_t* i
 	//whereas in an indirect call we are not
 	symtab_function_record_t* callee = instruction->called_function;
 
-	//Grab out this LR for reference later on
-	live_range_t* destination_lr = instruction->destination_register->associated_live_range;
+	//Grab out this LR for reference later on. Remember that this is nullable, so we 
+	//need to account for that
+	live_range_t* destination_lr = NULL;
+	//By default it's NO_REG, we will assign if it exists below
+	general_purpose_register_t destination_lr_reg = NO_REG;
+	
+	//Assign if it's not null
+	if(instruction->destination_register != NULL){
+		destination_lr = instruction->destination_register->associated_live_range;
+		//Save the register as well now that we know it exists
+		destination_lr_reg = destination_lr->reg;
+	}
 
 	//Start off with this as the last instruction
 	instruction_t* last_instruction = instruction;
@@ -2760,7 +2770,7 @@ static instruction_t* insert_caller_saved_logic_for_direct_call(instruction_t* i
 
 		//There's also no point in saving this. This could happen
 		//if we have precoloring
-		if(reg == destination_lr->reg){
+		if(reg == destination_lr_reg){
 			continue;
 		}
 
