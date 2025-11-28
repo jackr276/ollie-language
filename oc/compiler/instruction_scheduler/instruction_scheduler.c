aa03d6c4738cf04573b67b0c5a5c03d6b452d7f3
jackr276 <jack_m_robbins@me.com>
2025-11-28 15:35:43 -0500

    Rewrote, better approach now

--- oc/compiler/instruction_scheduler/instruction_scheduler.c
+++ oc/compiler/instruction_scheduler/instruction_scheduler.c
@@ -6,6 +6,7 @@
 
 #include "instruction_scheduler.h"
 #include "../data_dependency_graph/data_dependency_graph.h"
+#include <stdint.h>
 #include <sys/types.h>
 
 
@@ -22,176 +23,43 @@
  * ...
  * Given: addb t4, t5
  *
- * Then we need to establish a dependence between given and the candidate
+ * We are looking for only a specific variable here(say t4). Once we find that variable's
+ * definition, we're done for that given variable
  */
-static void update_dependence(instruction_t* given, instruction_t* candidate){
-	//The candidate never even assigns anything, so why bother checking
-	if(is_destination_assigned(candidate) == FALSE){
-		return;
-	}
+static void update_dependence_for_variable(instruction_t* given, instruction_t** instructions, three_addr_var_t* variable, u_int32_t start){
+	//Run through all of the instructions starting at what we were given
+	for(int32_t i = start; i >= 0; i--){
+		//Extract it
+		instruction_t* current = instructions[i];
+		
+		//If we don't even deal with the destination why bother
+		if(is_destination_assigned(current) == FALSE){
+			continue;
+		}
 
-	//We are looking to see if the candidate's destination register
-	//is used by us
-	three_addr_var_t* destination_register = candidate->destination_register;
+		//Grab these out
+		three_addr_var_t* destination = current->destination_register;
+		three_addr_var_t* destination2 = current->destination_register2;
 
-	//No point in going on here if there is no destination register to speak of
-	//in the candidate
-	if(destination_register == NULL){
-		return;
-	}
+		//If they're equal then we're good
+		if(variables_equal(destination, variable, FALSE) == TRUE){
+			//Given depends on current
+			add_dependence(current, given);
 
-	//Go for the source first
-	if(given->source_register != NULL){
-		//These variables are equal, we have a dependence
-		if(variables_equal(given->source_register, destination_register, FALSE) == TRUE){
-			//Given depends on candidate
-			add_dependence(candidate, given);
+			//We're done
 			return;
 		}
-	}
 
-	/**
-	 * Recall the unique procedures for the destination register
-	 *	
-	 *	1.) It can be a source & a destination(is_destination_also_source)
-	 *	2.) It can exclusively be a source(we ruled this out at the top)
-	 *	3.) It's just a destination, in which case we don't care(we'd just skip here)
-	 */
-	if(is_destination_also_operand(given) == TRUE){
-		//These variables are equal, we have a dependence
-		if(variables_equal(given->destination_register, destination_register, FALSE) == TRUE){
-			//Given depends on candidate
-			add_dependence(candidate, given);
+		//We're also done here
+		if(variables_equal(destination2, variable, FALSE) == TRUE){
+			//Given depends on current
+			add_dependence(current, given);
+
+			//We're done
 			return;
 		}
-	}
 
-	//Second source
-	if(given->source_register2 != NULL){
-		//These variables are equal, we have a dependence
-		if(variables_equal(given->source_register2, destination_register, FALSE) == TRUE){
-			//Given depends on candidate
-			add_dependence(candidate, given);
-			return;
-		}
-	}
-
-	//Address calc registers
-	if(given->address_calc_reg1 != NULL){
-		//These variables are equal, we have a dependence
-		if(variables_equal(given->address_calc_reg1, destination_register, FALSE) == TRUE){
-			//Given depends on candidate
-			add_dependence(candidate, given);
-			return;
-		}
-	}
-
-	//Address calc registers
-	if(given->address_calc_reg2 != NULL){
-		//These variables are equal, we have a dependence
-		if(variables_equal(given->address_calc_reg2, destination_register, FALSE) == TRUE){
-			//Given depends on candidate
-			add_dependence(candidate, given);
-			return;
-		}
-	}
-
-	//Now run through all of the parameters *if* they exist
-	if(given->parameters != NULL){
-		//Run through them all
-		dynamic_array_t* params = given->parameters;
-		for(u_int16_t i = 0; i < params->current_index; i++){
-			//Extract it
-			three_addr_var_t* param = dynamic_array_get_at(params, i);
-
-			//Add it and leave if so
-			if(variables_equal(param, destination_register, FALSE) == TRUE){
-				add_dependence(candidate, given);
-				return;
-			}
-		}
-	}
-
-
-	/**
-	 * Recall that there exist some instructions that have 2 destination registers.
-	 * If this is the case, we will need to perform all of this checking again
-	 */
-	three_addr_var_t* destination_register2 = given->destination_register2;
-	if(destination_register2 == NULL){
-		return;
-	}
-
-	//Go for the source first
-	if(given->source_register != NULL){
-		//These variables are equal, we have a dependence
-		if(variables_equal(given->source_register, destination_register2, FALSE) == TRUE){
-			//Given depends on candidate
-			add_dependence(candidate, given);
-			return;
-		}
-	}
-
-	/**
-	 * Recall the unique procedures for the destination register
-	 *	
-	 *	1.) It can be a source & a destination(is_destination_also_source)
-	 *	2.) It can exclusively be a source(we ruled this out at the top)
-	 *	3.) It's just a destination, in which case we don't care(we'd just skip here)
-	 */
-	if(is_destination_also_operand(given) == TRUE){
-		//These variables are equal, we have a dependence
-		if(variables_equal(given->destination_register, destination_register2, FALSE) == TRUE){
-			//Given depends on candidate
-			add_dependence(candidate, given);
-			return;
-		}
-	}
-
-	//Second source
-	if(given->source_register2 != NULL){
-		//These variables are equal, we have a dependence
-		if(variables_equal(given->source_register2, destination_register2, FALSE) == TRUE){
-			//Given depends on candidate
-			add_dependence(candidate, given);
-			return;
-		}
-	}
-
-	//Address calc registers
-	if(given->address_calc_reg1 != NULL){
-		//These variables are equal, we have a dependence
-		if(variables_equal(given->address_calc_reg1, destination_register2, FALSE) == TRUE){
-			//Given depends on candidate
-			add_dependence(candidate, given);
-			return;
-		}
-	}
-
-	//Address calc registers
-	if(given->address_calc_reg2 != NULL){
-		//These variables are equal, we have a dependence
-		if(variables_equal(given->address_calc_reg2, destination_register2, FALSE) == TRUE){
-			//Given depends on candidate
-			add_dependence(candidate, given);
-			return;
-		}
-	}
-
-	//Now run through all of the parameters *if* they exist
-	if(given->parameters != NULL){
-		//Run through them all
-		dynamic_array_t* params = given->parameters;
-		for(u_int16_t i = 0; i < params->current_index; i++){
-			//Extract it
-			three_addr_var_t* param = dynamic_array_get_at(params, i);
-
-			//Add it and leave if so
-			if(variables_equal(param, destination_register2, FALSE) == TRUE){
-				add_dependence(candidate, given);
-				return;
-			}
-		}
+		//Otherwise we just keep going
 	}
 }
 
@@ -214,14 +82,34 @@ static void build_dependency_graph_for_block(basic_block_t* block, instruction_t
 		//	2.) If they do get assigned in this block, what are those instructions that
 		//	are doing the assignment
 		
-		//Let's now backtrace from this current instruction up the chain. We can
-		//start at this given instruction's immediate predecessor
-		for(int32_t j = i - 1; j >= 0; j--){
-			//Extract it
-			instruction_t* candidate = instructions[j];
+		//For each variable in the instruction, we need to perform the search
+		if(is_destination_also_operand(current) == TRUE){
+			//Start searching here, beginngin at the last instruction
+			update_dependence_for_variable(current, instructions, current->destination_register, i - 1);
+		}
 
-			//Update the dependence
-			update_dependence(current, candidate);
+		//Same for the source
+		if(current->source_register != NULL){
+			//Start searching here, beginngin at the last instruction
+			update_dependence_for_variable(current, instructions, current->source_register, i - 1);
+		}
+
+		//Same for the second source
+		if(current->source_register2 != NULL){
+			//Start searching here, beginngin at the last instruction
+			update_dependence_for_variable(current, instructions, current->source_register2, i - 1);
+		}
+
+		//And the address calc registers
+		if(current->address_calc_reg1 != NULL){
+			//Start searching here, beginngin at the last instruction
+			update_dependence_for_variable(current, instructions, current->address_calc_reg1, i - 1);
+		}
+
+		//And the address calc registers
+		if(current->address_calc_reg2 != NULL){
+			//Start searching here, beginngin at the last instruction
+			update_dependence_for_variable(current, instructions, current->address_calc_reg2, i - 1);
 		}
 	}
 }
