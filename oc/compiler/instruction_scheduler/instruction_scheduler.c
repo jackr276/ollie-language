c7d46f575657a624a7f77bbe8f54c2669583f07a
jackr276 <jack_m_robbins@me.com>
2025-11-28 07:42:18 -0500

    Beginnings of scheduler API written

--- oc/compiler/instruction_scheduler/instruction_scheduler.c
+++ oc/compiler/instruction_scheduler/instruction_scheduler.c
@@ -5,3 +5,17 @@
 */
 
 #include "instruction_scheduler.h"
+
+
+
+/**
+ * Root level function that is exposed via the API
+ */
+cfg_t* schedule_all_instructions(cfg_t* cfg){
+
+
+	//Give back the final CFG. This is more symbolic
+	//than anything, the CFG itself has been modified by
+	//the whole procedure
+	return cfg;
+}
--- oc/compiler/instruction_scheduler/instruction_scheduler.h
+++ oc/compiler/instruction_scheduler/instruction_scheduler.h
@@ -2,18 +2,17 @@
  * Author: Jack Robbins
  *
  * This header defines the APIs for the instruction scheduler submodule
- *
- * NOTES:
- * The ollie compiler exclusively targets x86-64 processors. Since the modern variants of these processors are dynamically
- * scheduled, the instruction scheduler does not need to insert NOP operations
- *
- * NOTE: Since this is not an absolutely essential piece of the compiler, it has been omitted for this first cut
 */
 
 //Include guards
 #ifndef INSTRUCTION_SCHEDULER_H
 #define INSTRUCTION_SCHEDULER_H
+#include "../cfg/cfg.h"
 
-
+/**
+ * Expose the root level API to the front end. Obviously
+ * there is much more that goes on behind the scenes here with the sceduler
+ */
+cfg_t* schedule_all_instructions(cfg_t* cfg);
 
 #endif /* INSTRUCTION_SCHEDULER_H */
