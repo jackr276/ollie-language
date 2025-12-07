fb781f6c6ca239e936c9f383920bf84b89cb1592
jackr276 <jack_m_robbins@me.com>
2025-12-07 08:35:01 -0500

    Continue work on estimation

--- oc/compiler/utils/stack/nesting_stack.c
+++ oc/compiler/utils/stack/nesting_stack.c
@@ -63,7 +63,8 @@ u_int8_t nesting_stack_is_empty(nesting_stack_t* nesting_stack){
 
 /**
  * Get the estimated execution frequency of something given a nesting level using
- * our custom rules
+ * our custom rules. There's no hard and fast rule here - remember that this is only
+ * intended to provide a general estimate
  *
  * Algorithm:
  * 	for each level in the stack from bottom to top
@@ -72,7 +73,7 @@ u_int8_t nesting_stack_is_empty(nesting_stack_t* nesting_stack){
  * 		if level == loop
  * 			estimated_execution_frequency *= 10
  * 		if level == case_statement
- * 			estimated_execution_frequency *= 1/5
+ * 			estimated_execution_frequency *= 1/8
  */
 u_int32_t get_estimated_execution_frequency_from_nesting_stack(nesting_stack_t* stack){
 	//Initialize our frequency to be 1 initially
@@ -85,7 +86,32 @@ u_int32_t get_estimated_execution_frequency_from_nesting_stack(nesting_stack_t* 
 
 		//We update based on it
 		switch(level){
+			//We assume that a branch in an if-statement executes 
+			//roughly half of the time. We need to account for
+			//the case where we have a 1 here however
 			case NESTING_IF_STATEMENT:
+				//If this isn't 1, we can divide and not
+				//get 0
+				if(estimated_execution_frequency != 1){
+					//Divide by 2
+					estimated_execution_frequency >>= 1;
+				}
+
+				break;
+
+			//We assume that case statements execute
+			//roughly 1/8 of the time of their parents
+			//Again if the parent is more than 8, we can do
+			//this. If not, we just keep the current frequency
+			case NESTING_CASE_STATEMENT:
+			case NESTING_C_STYLE_CASE_STATEMENT:
+				//So long as we aren't zeroing stuff out. If we would,
+				//then we leave the top execution as what it was
+				if(estimated_execution_frequency >> 3 != 0){
+					estimated_execution_frequency >>= 3;
+				}
+
+				break;
 
 			//We assume that each loop executes 10 times
 			case NESTING_LOOP_STATEMENT:
