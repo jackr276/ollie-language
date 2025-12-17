a469a407db2ba395966db5549744affa29fe7b71
jackr276 <jack_m_robbins@me.com>
2025-12-17 05:51:03 -0500

    Input sanitation for struct initializers working, test case added

--- oc/compiler/parser/parser.c
+++ oc/compiler/parser/parser.c
@@ -8500,6 +8500,14 @@ static generic_type_t* validate_intializer_types(generic_type_t* target_type, ge
 			
 		//A struct initializer list also has it's own special checking function that we must use
 		case AST_NODE_TYPE_STRUCT_INITIALIZER_LIST:
+			//What if the user is trying to use an array initializer on a non-array type? If so, this should fail
+			if(target_type->type_class != TYPE_CLASS_STRUCT){
+				sprintf(info, "Type \"%s\" is not a struct and therefore may not be initialized with the {} syntax", target_type->type_name.string);
+				print_parse_message(PARSE_ERROR, info, parser_line_num);
+				//Null signifies failure
+				return NULL;
+			}
+
 			//Run the validation step for a struct
 			validation_succeeded = validate_types_for_struct_initializer_list(target_type, initializer_node, is_global);
 
