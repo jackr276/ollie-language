3bca022e9eb7c57feb9696e5fbb45a715e31ac17
jackr276 <jack_m_robbins@me.com>
2025-12-20 08:07:58 -0500

    Add backend support for left shifting constants

--- oc/compiler/ast/ast.h
+++ oc/compiler/ast/ast.h
@@ -250,6 +250,13 @@ void add_constant_nodes(generic_ast_node_t* constant_node1, generic_ast_node_t* 
 void right_shift_constant_nodes(generic_ast_node_t* constant_node1, generic_ast_node_t* constant_node2);
 
 /**
+ * Emit the left shift of two given constants. The result will overwrite the first constant given
+ *
+ * The result will be: constant1 = constant1 << constant2
+ */
+void left_shift_constant_nodes(generic_ast_node_t* constant_node1, generic_ast_node_t* constant_node2);
+
+/**
  * Emit the difference of two given constants. The result will overwrite the first constant given
  *
  * The result will be: constant1 = constant1 - constant2
