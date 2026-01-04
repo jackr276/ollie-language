ac963ef32acd44843afefc1b3b1db6c3a73fc36b
jackr276 <jack_m_robbins@me.com>
2026-01-04 07:51:03 -0500

    Full integration of float and double added

--- oc/compiler/ast/ast.c
+++ oc/compiler/ast/ast.c
@@ -3453,6 +3453,44 @@ void not_equals_constant_nodes(generic_ast_node_t* constant_node1, generic_ast_n
 
 			break;
 
+		case FLOAT_CONST:
+			//Now go based on the second one's type
+			switch(constant_node2->constant_type){
+				case DOUBLE_CONST:
+					constant_node1->constant_value.float_value = constant_node1->constant_value.float_value != constant_node2->constant_value.double_value;
+					break;
+				case LONG_CONST_FORCE_U:
+					constant_node1->constant_value.float_value = constant_node1->constant_value.float_value != constant_node2->constant_value.unsigned_long_value;
+					break;
+				case LONG_CONST:
+					 constant_node1->constant_value.float_value = constant_node1->constant_value.float_value != constant_node2->constant_value.signed_long_value;
+					break;
+				case INT_CONST_FORCE_U:
+					 constant_node1->constant_value.float_value = constant_node1->constant_value.float_value != constant_node2->constant_value.unsigned_int_value;
+					break;
+				case FLOAT_CONST:
+					constant_node1->constant_value.float_value = constant_node1->constant_value.float_value != constant_node2->constant_value.float_value;
+					break;
+				case INT_CONST:
+					constant_node1->constant_value.float_value = constant_node1->constant_value.float_value != constant_node2->constant_value.signed_int_value;
+					break;
+				case SHORT_CONST:
+					constant_node1->constant_value.float_value = constant_node1->constant_value.float_value != constant_node2->constant_value.signed_short_value;
+					break;
+				case SHORT_CONST_FORCE_U:
+					constant_node1->constant_value.float_value = constant_node1->constant_value.float_value != constant_node2->constant_value.unsigned_short_value;
+					break;
+				case CHAR_CONST:
+					constant_node1->constant_value.float_value = constant_node1->constant_value.float_value != constant_node2->constant_value.char_value;
+					break;
+				//This should never happen
+				default:
+					printf("Fatal internal compiler error: Unsupported constant != operation\n");
+					exit(1);
+			}
+
+			break;
+
 		case LONG_CONST_FORCE_U:
 			//Now go based on the second one's type
 			switch(constant_node2->constant_type){
@@ -3528,6 +3566,45 @@ void not_equals_constant_nodes(generic_ast_node_t* constant_node1, generic_ast_n
 			}
 
 			break;
+
+		case DOUBLE_CONST:
+			//Now go based on the second one's type
+			switch(constant_node2->constant_type){
+				case DOUBLE_CONST:
+					constant_node1->constant_value.double_value = constant_node1->constant_value.double_value != constant_node2->constant_value.double_value;
+					break;
+				case LONG_CONST_FORCE_U:
+					constant_node1->constant_value.double_value = constant_node1->constant_value.double_value != constant_node2->constant_value.unsigned_long_value;
+					break;
+				case LONG_CONST:
+					 constant_node1->constant_value.double_value = constant_node1->constant_value.double_value != constant_node2->constant_value.signed_long_value;
+					break;
+				case INT_CONST_FORCE_U:
+					 constant_node1->constant_value.double_value = constant_node1->constant_value.double_value != constant_node2->constant_value.unsigned_int_value;
+					break;
+				case FLOAT_CONST:
+					constant_node1->constant_value.double_value = constant_node1->constant_value.double_value != constant_node2->constant_value.float_value;
+					break;
+				case INT_CONST:
+					constant_node1->constant_value.double_value = constant_node1->constant_value.double_value != constant_node2->constant_value.signed_int_value;
+					break;
+				case SHORT_CONST:
+					constant_node1->constant_value.double_value = constant_node1->constant_value.double_value != constant_node2->constant_value.signed_short_value;
+					break;
+				case SHORT_CONST_FORCE_U:
+					constant_node1->constant_value.double_value = constant_node1->constant_value.double_value != constant_node2->constant_value.unsigned_short_value;
+					break;
+				case CHAR_CONST:
+					constant_node1->constant_value.double_value = constant_node1->constant_value.double_value != constant_node2->constant_value.char_value;
+					break;
+				//This should never happen
+				default:
+					printf("Fatal internal compiler error: Unsupported constant != operation\n");
+					exit(1);
+			}
+
+			break;
+
 
 		case SHORT_CONST:
 			//Now go based on the second one's type
