34fec5f0027cfdbbc31662080b273f601e4ae3e1
jackr276 <jack_m_robbins@me.com>
2026-02-03 06:46:44 -0500

    Very very beginning of the preprocessor

--- oc/compiler/preprocessor/preprocessor.h
+++ oc/compiler/preprocessor/preprocessor.h
@@ -10,5 +10,35 @@
 #ifndef PREPROCESSOR_H
 #define PREPROCESSOR_H
 
+//Really all that is needed here is the lexer
+#include "../lexer/lexer.h"
+#include <sys/types.h>
+
+//The generic result struct for the preprocessor
+typedef struct preprocessor_results_t preprocessor_results_t;
+
+/**
+ * Hold onto some info from the preprocessor like the status,
+ * token stream, and macros processed
+ */
+struct preprocessor_results_t {
+	//The token stream
+	ollie_token_stream_t* stream;
+	//The number of errors
+	u_int32_t error_count;
+	//The number of warnings
+	u_int32_t warning_count;
+	//The number of macros processed(more of novelty info but it's fine to have)
+	u_int32_t macros_processed;
+};
+
+
+/**
+ * Entry point to the entire preprocessor is here. The preprocessor
+ * will traverse the token stream and make replacements as it sees
+ * fit with defined macros
+ */
+preprocessor_results_t preprocess(ollie_token_stream_t* stream);
+
 
 #endif /* PREPROCESSOR_H */
