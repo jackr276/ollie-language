5ff957a06450f2119252360476ef9ef876bb003f
jackr276 <jack_m_robbins@me.com>
2026-02-03 18:38:48 -0500

    Rework the way that the token array header file works

--- oc/compiler/utils/ollie_token_array/ollie_token_array.h
+++ oc/compiler/utils/ollie_token_array/ollie_token_array.h
@@ -8,9 +8,9 @@
 #ifndef OLLIE_TOKEN_ARRAY_H 
 #define OLLIE_TOKEN_ARRAY_H 
 
-#include "../../lexer/lexer.h"
-
 #include <sys/types.h>
+//Link to the token/lexitem structs
+#include "../token.h"
 
 typedef struct ollie_token_array_t ollie_token_array_t;
 
@@ -21,104 +21,92 @@ typedef struct ollie_token_array_t ollie_token_array_t;
 */
 struct ollie_token_array_t{
 	//The internal token array
-	lex** internal_array;
+	lexitem_t* internal_array;
 	//The current maximum size
-	u_int16_t current_max_size;
+	u_int32_t current_max_size;
 	//The current index that we're on - it also happens to be
 	//how many nodes we have
-	u_int16_t current_index;
+	u_int32_t current_index;
 };
 
+
 /**
- * Initialize a dynamic array. The resulting
+ * Initialize a token array. The resulting
  * control structure will be stack allocated
  */
-dynamic_array_t dynamic_array_alloc();
-
+ollie_token_array_t token_array_alloc();
 
 /**
- * Initialize a dynamic array with an initial
+ * Initialize a token array with an initial
  * size. This is useful if we already know
  * the size we need
  */
-dynamic_array_t dynamic_array_alloc_initial_size(u_int16_t initial_size);
-
+ollie_token_array_t token_array_alloc_initial_size(u_int32_t initial_size);
 
 /**
- * Create an exact clone of the dynamic array that we're given
+ * Create an exact clone of the token array that we're given
  */
-dynamic_array_t clone_dynamic_array(dynamic_array_t* array);
+ollie_token_array_t clone_token_array(ollie_token_array_t* array);
 
 /**
- * Does the dynamic array contain this pointer?
+ * Does the token array contain this pointer?
  * 
  * RETURNS: the index if true, -1 if not
 */
-int16_t dynamic_array_contains(dynamic_array_t* array, void* ptr);
-
+int16_t token_array_contains(ollie_token_array_t* array, lexitem_t* lexitem);
 
 /**
- * Is the dynamic array is empty?
+ * Is the token array is empty?
 */
-u_int8_t dynamic_array_is_empty(dynamic_array_t* array);
-
+u_int8_t token_array_is_empty(ollie_token_array_t* array);
 
 /**
- * Add an item into the dynamic array
+ * Add an item into the array. Note that we pass by copy for convenience, but we are
+ * not storing pointers in the array
  */
-void dynamic_array_add(dynamic_array_t* array, void* ptr);
+void token_array_add(ollie_token_array_t* array, lexitem_t* lexitem);
 
 /**
- * Clear a dynamic array entirely - keeps the size unchanged, but
+ * Clear a token array entirely - keeps the size unchanged, but
  * sets the entire internal array to 0
  */
-void clear_dynamic_array(dynamic_array_t* array);
+void clear_token_array(ollie_token_array_t* array);
 
 /**
  * Get an element at a specified index. Do not remove the element
+ *
+ * Returns a copy of the specified element
  */
-void* dynamic_array_get_at(dynamic_array_t* array, u_int16_t index);
+lexitem_t token_array_get_at(ollie_token_array_t* array, u_int32_t index);
 
+/**
+ * Get a pointer to an element at a given index. Do not remove the element
+ */
+lexitem_t* token_array_get_pointer_at(ollie_token_array_t* array, u_int32_t index);
 
 /**
  * Set an element at a specified index. No check will be performed
  * to see if the element is already there. Dynamic resize
  * will be in effect here
  */
-void dynamic_array_set_at(dynamic_array_t* array, void* ptr, u_int16_t index);
-
+void token_array_set_at(ollie_token_array_t* array, lexitem_t* lexitem, u_int32_t index);
 
 /**
- * Delete an element from the dynamic array at a given index. Returns
+ * Delete an element from the token array at a given index. Returns
  * the element at said index
  */
-void* dynamic_array_delete_at(dynamic_array_t* array, u_int16_t index);
+lexitem_t token_array_delete_at(ollie_token_array_t* array, u_int32_t index);
 
 /**
  * Delete the pointer itself from the dynamic array
  *
  * Will not complain if it cannot be found - it simply won't be deleted
  */
-void dynamic_array_delete(dynamic_array_t* array, void* ptr);
+void token_array_delete(ollie_token_array_t* array, lexitem_t* lexitem);
 
 /**
- * Remove an element from the back of the dynamic array - O(1) removal
+ * Deallocate an entire token array. 
  */
-void* dynamic_array_delete_from_back(dynamic_array_t* array);
-
-/**
- * Are two dynamic arrays completely equal? A "deep equals" 
- * will ensure that every single element in one array is also inside of the
- * other, and that no elements in one array are different
- */
-u_int8_t dynamic_arrays_equal(dynamic_array_t* a, dynamic_array_t* b);
-
-/**
- * Deallocate an entire dynamic array. 
- *
- * NOTE: This will not touch/free any pointers in the array itself,
- * just the overall structure
-*/
-void dynamic_array_dealloc(dynamic_array_t* array);
+void token_array_dealloc(ollie_token_array_t* array);
 
 #endif /* OLLIE_TOKEN_ARRAY_H */
