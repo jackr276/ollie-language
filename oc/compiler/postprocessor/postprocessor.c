02e9c8784eb27e5eae89366ff4015b854b7e6fd8
jackr276 <jack_m_robbins@me.com>
2026-02-10 18:17:37 -0500

    Begin implementations for the branch reducer in the postprocessor

--- oc/compiler/postprocessor/postprocessor.c
+++ oc/compiler/postprocessor/postprocessor.c
@@ -88,6 +88,33 @@ static instruction_t* combine_blocks(cfg_t* cfg, basic_block_t* a, basic_block_t
 
 
 /**
+ * A helper function to determine if something is or is not
+ * a jump
+ */
+static inline u_int8_t is_jump_instruction(instruction_t* instruction){
+	switch(instruction->instruction_type){
+		case JMP:
+		case JNE:
+		case JE:
+		case JNZ:
+		case JZ:
+		case JGE:
+		case JG:
+		case JLE:
+		case JL:
+		case JA:
+		case JP:
+		case JAE:
+		case JB:
+		case JBE:
+			return TRUE;
+		default:
+			return FALSE;
+	}
+}
+
+
+/**
  * Post register allocation, it is possible that the register allocator
  * could've given us something like: movq %rax, %rax. This is entirely
  * useless, and as such we will eliminate instructions like these
@@ -384,6 +411,10 @@ static inline u_int8_t does_block_contain_more_than_one_jump_to_target(basic_blo
  *
  * Procedure branch_reduce_postprocess():
  * 	for each block in postorder
+ * 		if i ends in a conditional branch
+ * 			if both targets are identical then
+ * 				replace branch with a jump to said block
+ *
  * 		if i ends in a jump to j then
  * 			if i is empty then
  * 				replace transfers to i with transfers to j
@@ -409,6 +440,16 @@ static u_int8_t branch_reduce_postprocess(cfg_t* cfg, dynamic_array_t* postorder
 		 */
 		if(current->exit_statement != NULL
 			&& current->exit_statement->instruction_type == JMP){
+			
+			//Holders for the exit statement and prior instruction
+			instruction_t* exit_statement = current->exit_statement;
+			instruction_t* second_to_last_statement = exit_statement->previous_statement;
+
+			if(second_to_last_statement != NULL
+				&& is_)
+		
+
+
 			//Extract the block(j) that we're going to
 			basic_block_t* jumping_to_block = current->exit_statement->if_block;
 
