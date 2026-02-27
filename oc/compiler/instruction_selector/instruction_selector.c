8d5676271fd203e1e083858b058a433582e8fcb2
jackr276 <jack_m_robbins@me.com>
2026-02-27 06:39:59 -0500

    Begin to completely reorganize the constant system

--- oc/compiler/instruction_selector/instruction_selector.c
+++ oc/compiler/instruction_selector/instruction_selector.c
@@ -761,178 +761,16 @@ static inline u_int8_t binary_operator_valid_for_inplace_constant_match(ollie_to
  * Remediate a memory address that is *not* in a memory access(load or store) context. This will primarily
  * be hit when we're taking memory addresses or doing pointer arithmetic with arrays
  */
-static void remediate_memory_address_in_non_access_context(instruction_window_t* window, instruction_t* instruction){
+static void remediate_memory_address_variable_in_non_access_context(instruction_window_t* window, instruction_t* instruction){
 	//Grab this out
 	symtab_variable_record_t* var = instruction->op1->linked_var;
 
 	/**
-	 * Handle a standard case - we have a variable that is going to be an address
-	 * on a stack
-	 */
-	if(var->membership != GLOBAL_VARIABLE){
-		//We have no stack region - this likely means that it's a
-		//reference parameter of some kind. In this case, we will *remove*
-		//the special memory type of this paramter and just let it use
-		//the variable as normal
-		if(var->stack_region == NULL){
-			//Remediate the type here
-			instruction->op1->variable_type = VARIABLE_TYPE_NON_TEMP;
-
-			//And just let it go now 
-			return;
-		}
-
-
-		//Extract the stack offset for our use. This will determine how 
-		//we process things down below
-		int64_t stack_offset = var->stack_region->function_local_base_address;
-
-		//Go based on what kind of statement that we've got here
-		switch(instruction->statement_type){
-			//If we have an assignment statement, we
-			//can turn this into a lea with an offset or a
-			//straight assignment depending on the offset
-			case THREE_ADDR_CODE_ASSN_STMT:
-				//Make it a lea
-				if(stack_offset != 0){
-					instruction->statement_type = THREE_ADDR_CODE_LEA_STMT;
-
-					//Op1 becomes that stack pointer
-					instruction->op1 = stack_pointer_variable;
-
-					//And op1_const is our offset
-					instruction->op1_const = emit_direct_integer_or_char_constant(stack_offset, u64);
-
-					//This is a lea with an offset only
-					instruction->lea_statement_type = OIR_LEA_TYPE_OFFSET_ONLY;
-
-				//Otherwise, we'll just swap the var out with the stack pointer since
-				//they're one in the same
-				} else {
-					instruction->op1 = stack_pointer_variable;
-				}
-
-				break;
-
-			//For a statement like this, we will merge the existing
-			//constant in. We know that the only two possible operands
-			//with a memory address are Plus/minus, so we only need to 
-			//account for 2 cases here
-			case THREE_ADDR_CODE_BIN_OP_WITH_CONST_STMT:
-				//Make it a lea
-				if(stack_offset != 0){
-					//Emit the constant
-					three_addr_const_t* lea_constant = emit_direct_integer_or_char_constant(stack_offset, i64);
-
-					//Simplify based on what we have
-					switch(instruction->op){
-						case PLUS:
-							add_constants(lea_constant, instruction->op1_const);
-							break;
-
-						case MINUS:
-							subtract_constants(lea_constant, instruction->op1_const);
-							break;
-
-						//This should be impossible, if we get here it's a hard out
-						default:
-							printf("Fatal internal compiler error. Attempt to do a binary operation that is not +/- with a memory address\n");
-							exit(1);
-					}
-
-					//Wipe out the operator
-					instruction->op = BLANK;
-
-					//Op1 becomes that stack pointer
-					instruction->op1 = stack_pointer_variable;
-
-					//Op1 const is the lea constant
-					instruction->op1_const = lea_constant;
-
-					//Change the instruction type to a lea
-					instruction->statement_type = THREE_ADDR_CODE_LEA_STMT;
-
-					//This is an offset only
-					instruction->lea_statement_type = OIR_LEA_TYPE_OFFSET_ONLY;
-
-				//Otherwise, we'll just swap the var out with the stack pointer since
-				//they're one in the same
-				} else {
-					instruction->op1 = stack_pointer_variable;
-				}
-
-				break;
-
-			//Final and trickiest case. We need to have a memory calculation *and* a regular
-			//calculation stuffed into here, but we only have 2 operands to work with. We will
-			//need to use our special version of a lea for this in most cases
-			case THREE_ADDR_CODE_BIN_OP_STMT:
-				//Make it a lea, we'll need to use op2
-				//for the second variable
-				if(stack_offset != 0){
-					//Create the offset constant
-					three_addr_const_t* stack_offset_constant = emit_direct_integer_or_char_constant(stack_offset, i64);
-
-					//This is now our op1_const
-					instruction->op1_const = stack_offset_constant;
-
-					//Op1 becomes the stack pointer
-					instruction->op1 = stack_pointer_variable;
-
-					//Finally declare that this is a lea statement
-					instruction->statement_type = THREE_ADDR_CODE_LEA_STMT;
-
-					//Go based on the op here
-					switch(instruction->op){
-						//In this case, we'd have something like t5 <- <offset>(t4, t5)
-						case PLUS:
-							//This is a lea statement with registers and an offset
-							instruction->lea_statement_type = OIR_LEA_TYPE_REGISTERS_AND_OFFSET;
-							
-							//Nothing else to do here
-							break;
-						
-						//For a minus, we'll need to circumvent the system by using a -1 multiplier
-						//to make this still work for our lea. Since we have op1 - op2, we can rewrite
-						//this into op1 + op2 * -1
-						case MINUS:
-							//Full stack here
-							instruction->lea_statement_type = OIR_LEA_TYPE_REGISTERS_OFFSET_AND_SCALE;
-
-							//-1 to mimic the subtraction
-							instruction->lea_multiplier = -1;
-
-							break;
-						
-						//Unreachable path - hard fail if we somehow get to this
-						default:
-							printf("Fatal internal compiler error: Invalid binary operand found on address calculation\n");
-							exit(1);
-					}
-
-					//Wipe out the op once we're done
-					instruction->op = BLANK;
-					
-				//Then again all we need to do here is set the op1
-				//to be our stack pointer
-				} else {
-					instruction->op1 = stack_pointer_variable;
-				}
-
-				break;
-
-			//This should never happen
-			default:
-				printf("Fatal internal compiler error: unreachable path hit in memory address remediation\n");
-				exit(1);
-		}
-	
-	/**
-	 * Otherwise it is a global variable, and we will treat it as such. Global variables will generate 2 instructions on most occassions
+	 * Special handling if this is a global variable. Global variables will generate 2 instructions on most occassions
 	 * the lea instruction to grab the address and then the actual address manipulation in the binary operation. Note that for these steps,
 	 * window reconstruction is required
 	 */
-	} else {
+	if(var->membership == GLOBAL_VARIABLE){
 		//The global variable address calculation
 		instruction_t* global_var_address_instruction;
 
@@ -999,6 +837,183 @@ static void remediate_memory_address_in_non_access_context(instruction_window_t*
 				printf("Fatal internal compiler error: unreachable path hit in memory address remediation\n");
 				exit(1);
 		}
+
+		//We're completely done once we get here
+		return;
+	}
+
+	//
+	//reference parameter of some kind. In this case, we will *remove*
+	//the special memory type of this paramter and just let it use
+	//the variable as normal
+	//
+	//
+	//
+	//TODO DELETEME
+	if(var->stack_region == NULL){
+		//Remediate the type here
+		instruction->op1->variable_type = VARIABLE_TYPE_NON_TEMP;
+
+		//And just let it go now 
+		return;
+	}
+
+	switch(var->membership){
+		case VARIABLE_TYPE_MEMORY_ADDRESS:
+			break;
+
+		case VARIABLE_TYPE_STACK_PARAM_MEMORY_ADDRESS:
+			break;
+
+		//This should be impossible
+		default:
+			printf("Fatal internal compiler error: invalid variable membership found in memory address remediator\n");
+			exit(1);
+	}
+
+
+	//Extract the stack offset for our use. This will determine how 
+	//we process things down below
+	int64_t stack_offset = var->stack_region->function_local_base_address;
+
+	//Go based on what kind of statement that we've got here
+	switch(instruction->statement_type){
+		//If we have an assignment statement, we
+		//can turn this into a lea with an offset or a
+		//straight assignment depending on the offset
+		case THREE_ADDR_CODE_ASSN_STMT:
+			//Make it a lea
+			if(stack_offset != 0){
+				instruction->statement_type = THREE_ADDR_CODE_LEA_STMT;
+
+				//Op1 becomes that stack pointer
+				instruction->op1 = stack_pointer_variable;
+
+				//And op1_const is our offset
+				instruction->op1_const = emit_direct_integer_or_char_constant(stack_offset, u64);
+
+				//This is a lea with an offset only
+				instruction->lea_statement_type = OIR_LEA_TYPE_OFFSET_ONLY;
+
+			//Otherwise, we'll just swap the var out with the stack pointer since
+			//they're one in the same
+			} else {
+				instruction->op1 = stack_pointer_variable;
+			}
+
+			break;
+
+		//For a statement like this, we will merge the existing
+		//constant in. We know that the only two possible operands
+		//with a memory address are Plus/minus, so we only need to 
+		//account for 2 cases here
+		case THREE_ADDR_CODE_BIN_OP_WITH_CONST_STMT:
+			//Make it a lea
+			if(stack_offset != 0){
+				//Emit the constant
+				three_addr_const_t* lea_constant = emit_direct_integer_or_char_constant(stack_offset, i64);
+
+				//Simplify based on what we have
+				switch(instruction->op){
+					case PLUS:
+						add_constants(lea_constant, instruction->op1_const);
+						break;
+
+					case MINUS:
+						subtract_constants(lea_constant, instruction->op1_const);
+						break;
+
+					//This should be impossible, if we get here it's a hard out
+					default:
+						printf("Fatal internal compiler error. Attempt to do a binary operation that is not +/- with a memory address\n");
+						exit(1);
+				}
+
+				//Wipe out the operator
+				instruction->op = BLANK;
+
+				//Op1 becomes that stack pointer
+				instruction->op1 = stack_pointer_variable;
+
+				//Op1 const is the lea constant
+				instruction->op1_const = lea_constant;
+
+				//Change the instruction type to a lea
+				instruction->statement_type = THREE_ADDR_CODE_LEA_STMT;
+
+				//This is an offset only
+				instruction->lea_statement_type = OIR_LEA_TYPE_OFFSET_ONLY;
+
+			//Otherwise, we'll just swap the var out with the stack pointer since
+			//they're one in the same
+			} else {
+				instruction->op1 = stack_pointer_variable;
+			}
+
+			break;
+
+		//Final and trickiest case. We need to have a memory calculation *and* a regular
+		//calculation stuffed into here, but we only have 2 operands to work with. We will
+		//need to use our special version of a lea for this in most cases
+		case THREE_ADDR_CODE_BIN_OP_STMT:
+			//Make it a lea, we'll need to use op2
+			//for the second variable
+			if(stack_offset != 0){
+				//Create the offset constant
+				three_addr_const_t* stack_offset_constant = emit_direct_integer_or_char_constant(stack_offset, i64);
+
+				//This is now our op1_const
+				instruction->op1_const = stack_offset_constant;
+
+				//Op1 becomes the stack pointer
+				instruction->op1 = stack_pointer_variable;
+
+				//Finally declare that this is a lea statement
+				instruction->statement_type = THREE_ADDR_CODE_LEA_STMT;
+
+				//Go based on the op here
+				switch(instruction->op){
+					//In this case, we'd have something like t5 <- <offset>(t4, t5)
+					case PLUS:
+						//This is a lea statement with registers and an offset
+						instruction->lea_statement_type = OIR_LEA_TYPE_REGISTERS_AND_OFFSET;
+						
+						//Nothing else to do here
+						break;
+					
+					//For a minus, we'll need to circumvent the system by using a -1 multiplier
+					//to make this still work for our lea. Since we have op1 - op2, we can rewrite
+					//this into op1 + op2 * -1
+					case MINUS:
+						//Full stack here
+						instruction->lea_statement_type = OIR_LEA_TYPE_REGISTERS_OFFSET_AND_SCALE;
+
+						//-1 to mimic the subtraction
+						instruction->lea_multiplier = -1;
+
+						break;
+					
+					//Unreachable path - hard fail if we somehow get to this
+					default:
+						printf("Fatal internal compiler error: Invalid binary operand found on address calculation\n");
+						exit(1);
+				}
+
+				//Wipe out the op once we're done
+				instruction->op = BLANK;
+				
+			//Then again all we need to do here is set the op1
+			//to be our stack pointer
+			} else {
+				instruction->op1 = stack_pointer_variable;
+			}
+
+			break;
+
+		//This should never happen
+		default:
+			printf("Fatal internal compiler error: unreachable path hit in memory address remediation\n");
+			exit(1);
 	}
 }
 
@@ -1056,9 +1071,14 @@ static u_int8_t simplify_window(instruction_window_t* window){
 		case THREE_ADDR_CODE_ASSN_STMT:
 		case THREE_ADDR_CODE_BIN_OP_STMT:
 		case THREE_ADDR_CODE_BIN_OP_WITH_CONST_STMT:
-			//If it is a memory address, then we'll do this
-			if(first->op1->variable_type == VARIABLE_TYPE_MEMORY_ADDRESS){
-				remediate_memory_address_in_non_access_context(window, first);
+			//If we have any memory addresses now is the time
+			switch(first->op1->variable_type){
+				case VARIABLE_TYPE_MEMORY_ADDRESS:
+				case VARIABLE_TYPE_STACK_PARAM_MEMORY_ADDRESS:
+					remediate_memory_address_variable_in_non_access_context(window, first);
+					break;
+				default:
+					break;
 			}
 			
 			break;
@@ -1073,9 +1093,14 @@ static u_int8_t simplify_window(instruction_window_t* window){
 		case THREE_ADDR_CODE_ASSN_STMT:
 		case THREE_ADDR_CODE_BIN_OP_STMT:
 		case THREE_ADDR_CODE_BIN_OP_WITH_CONST_STMT:
-			//If it is a memory address, then we'll do this
-			if(second->op1->variable_type == VARIABLE_TYPE_MEMORY_ADDRESS){
-				remediate_memory_address_in_non_access_context(window, second);
+			//If we have any memory addresses now is the time
+			switch(second->op1->variable_type){
+				case VARIABLE_TYPE_MEMORY_ADDRESS:
+				case VARIABLE_TYPE_STACK_PARAM_MEMORY_ADDRESS:
+					remediate_memory_address_variable_in_non_access_context(window, second);
+					break;
+				default:
+					break;
 			}
 			
 			break;
@@ -1092,9 +1117,14 @@ static u_int8_t simplify_window(instruction_window_t* window){
 			case THREE_ADDR_CODE_ASSN_STMT:
 			case THREE_ADDR_CODE_BIN_OP_STMT:
 			case THREE_ADDR_CODE_BIN_OP_WITH_CONST_STMT:
-				//If it is a memory address, then we'll do this
-				if(third->op1->variable_type == VARIABLE_TYPE_MEMORY_ADDRESS){
-					remediate_memory_address_in_non_access_context(window, third);
+				//If we have any memory addresses now is the time
+				switch(third->op1->variable_type){
+					case VARIABLE_TYPE_MEMORY_ADDRESS:
+					case VARIABLE_TYPE_STACK_PARAM_MEMORY_ADDRESS:
+						remediate_memory_address_variable_in_non_access_context(window, third);
+						break;
+					default:
+						break;
 				}
 				
 				break;
