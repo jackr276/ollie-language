998aa869f4ce8827f6c7893f85b84bbdf2b5910d
jackr276 <jack_m_robbins@me.com>
2026-04-12 07:16:08 -0400

    Add new hash table utility, not yet fleshed out

---
+++ oc/compiler/utils/hashtable/hashtable.c
@@ -0,0 +1,4 @@
+/**
+ * Author: Jack Robbins
+ * This file contains the implementations for the APIs laid out in hashtable.h
+ */
---
+++ oc/compiler/utils/hashtable/hashtable.h
@@ -0,0 +1,13 @@
+/**
+ * Author: Jack Robbins
+ * This module contains API definitions for a basic hash table used by the compiler. The hash table is most
+ * often used for global value numbering optimizations
+ */
+
+
+#ifndef HASH_TABLE_H
+#define HASH_TABLE_H
+
+
+#endif /* HASH_TABLE_H */
+
