bb7e3b049e59ac94ac9c824c7edbcf61e338abc9
jackr276 <jack_m_robbins@me.com>
2026-04-25 07:27:23 -0400

    Continue work on scaffolding, want to make this recursive

--- oc/compiler/cfg/cfg.c
+++ oc/compiler/cfg/cfg.c
@@ -3240,10 +3240,51 @@ static cfg_result_package_t emit_branch_v2(basic_block_t* starting_block, generi
 		}
 
 	/**
-	 * If we got here then we are short circuiting TODO
+	 * If we got here then we are short circuiting based on the operator that we were given. The
+	 * short circuit process will create several additional blocks that we will use instead of 
+	 * just using this one block
+	 *
+	 * TODO JUST BASE CASE FOR NOW
 	 */
 	} else {
+		//Create our secondary block to use
+		basic_block_t* secondary_block = basic_block_alloc_and_estimate();
 
+		/**
+		 * We'll need to account for both the inverse and normal
+		 * branch cases whilst doing this, but the basic flow is as
+		 * follows
+		 *
+		 * Optimizing a logical and short circuit:
+		 * 	1.) Emit the left side binary expression entirely in the first block
+		 * 	2.) Branch out to the success/fail block as needed, and create a new block
+		 * 	3.) Emit the right side binary expression in the new block
+		 * 	4.) Emit the final branch nonsense in the new block
+		 */
+		if(conditional_node->binary_operator == DOUBLE_AND){
+			/**
+			 *
+			 */
+			if(branch_category == BRANCH_CATEGORY_NORMAL){
+
+			} else {
+
+			}
+
+
+		/**
+		 * Optimizing a logical or short circuit
+		 */
+		} else {
+			/**
+			 *
+			 */
+			if(branch_category == BRANCH_CATEGORY_NORMAL){
+
+			} else {
+
+			}
+		}
 	}
 
 
