8471879d243c888137bd3c2364983f389413a1fc
jackr276 <jack_m_robbins@me.com>
2026-04-28 17:09:33 -0400

    Continue work on redo

--- oc/compiler/cfg/cfg.c
+++ oc/compiler/cfg/cfg.c
@@ -10905,15 +10905,46 @@ static cfg_result_package_t emit_simple_initialization(basic_block_t* current_bl
 	 * emit a store operation
 	 */
 	} else if(let_variable->linked_var == NULL || let_variable->linked_var->stack_variable == FALSE){
-		//If it's not a binary operation then we'll just assign over
-		if(is_binary_operation(current_block->exit_statement) == FALSE){
-			printf("HERE\n\n\n\n");
+		//Final assignment if we need it
+		instruction_t* assignment_statement;
 
+		/**
+		 * If we have an exit statement *and* we are dealing with what the final_op1 is, we may
+		 * be able to shrink our footprint here
+		 */
+		if(current_block->exit_statement != NULL
+			&& variables_equal_no_ssa(final_op1, current_block->exit_statement->assignee, FALSE) == TRUE){
+
+			switch(current_block->exit_statement->statement_type){
+
+				case THREE_ADDR_CODE_BIN_OP_STMT:
+				case THREE_ADDR_CODE_BIN_OP_WITH_CONST_STMT:
+
+
+				default:
+					//The actual statement is the assignment of right to left
+					assignment_statement = emit_assignment_instruction(let_variable, final_op1);
+
+					//Finally we'll add this into the overall block
+					add_statement(current_block, assignment_statement);
+			}
+
+		/**
+		 * No fancy optimizations here - just emit an assignment over and we'll be
+		 * fine here
+		 */
+		} else {
 			//The actual statement is the assignment of right to left
 			instruction_t* assignment_statement = emit_assignment_instruction(let_variable, final_op1);
 
 			//Finally we'll add this into the overall block
 			add_statement(current_block, assignment_statement);
+		}
+
+		//If it's not a binary operation then we'll just assign over
+		if(is_binary_operation(current_block->exit_statement) == FALSE){
+			printf("HERE\n\n\n\n");
+
 
 		//If it is then we can just hijack the statement and replace it's variable with ours
 		} else {
