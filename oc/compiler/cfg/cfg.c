a5f440ed1ec185d6d02d406fb3ef0e6599af8d1a
jackr276 <jack_m_robbins@me.com>
2026-05-05 16:59:30 -0400

    Solution is good but we need to expand for anything that has a stack param

--- oc/compiler/cfg/cfg.c
+++ oc/compiler/cfg/cfg.c
@@ -7028,6 +7028,20 @@ static inline void handle_elaborative_stack_param_storage(basic_block_t* basic_b
 					 * Everything else we perform a normal store. There is no copy assignment required to make this happen
 					 */
 					default:
+						/**
+						 * If we have a memory address type, we'll need to emit an assignment here as we can't
+						 * have the memory address directly in the load
+						 */
+						if(result_var->variable_type == VARIABLE_TYPE_MEMORY_ADDRESS
+							|| result_var->variable_type == VARIABLE_TYPE_STACK_PARAM_MEMORY_ADDRESS){
+							instruction_t* assignment = emit_assignment_instruction(emit_temp_var(result_var->type), result_var);
+
+							add_statement(basic_block, assignment);
+
+							//This is the new result var
+							result_var = assignment->assignee;
+						}
+
 						//Create this one's stack region
 						variable_result_region = create_stack_region_for_type(stack_passed_parameters, result_var->type);
 
@@ -7279,6 +7293,9 @@ static cfg_result_package_t emit_function_call(basic_block_t* basic_block, gener
 	//Now that we have all of this, we need to go through and emit our final assignments for the function calls
 	//themselves
 	for(u_int32_t i = 0; i < non_elaborative_parameter_results.current_index; i++){
+		//For any result vars we need to grab
+		three_addr_var_t* result_var;
+
 		//For any/all call side regions that we need
 		stack_region_t* call_side_region;
 
@@ -7396,7 +7413,29 @@ static cfg_result_package_t emit_function_call(basic_block_t* basic_block, gener
 								break;
 
 							case PARAM_RESULT_TYPE_VAR:
-								store_operation = emit_store_with_constant_offset_ir_code(stack_pointer_variable, stack_offset, result->param_result.variable_result, parameter_type);
+								//Extract for convenience
+								result_var = result->param_result.variable_result;
+
+								/**
+								 * If we have a memory address type, we'll need to emit an assignment here as we can't
+								 * have the memory address directly in the load
+								 */
+								if(result_var->variable_type == VARIABLE_TYPE_MEMORY_ADDRESS
+									|| result_var->variable_type == VARIABLE_TYPE_STACK_PARAM_MEMORY_ADDRESS){
+									instruction_t* assignment = emit_assignment_instruction(emit_temp_var(result_var->type), result_var);
+
+									add_statement(basic_block, assignment);
+
+									//This is the first assignment if it's NULL
+									if(first_assignment_instruction == NULL){
+										first_assignment_instruction = assignment;
+									}
+
+									//This is the new result var
+									result_var = assignment->assignee;
+								}
+
+								store_operation = emit_store_with_constant_offset_ir_code(stack_pointer_variable, stack_offset, result_var, parameter_type);
 								break;
 						}
 
