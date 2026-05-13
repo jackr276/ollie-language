88c4407e1b8d8cb2696fede6e0266b3637158684
jackr276 <jack_m_robbins@me.com>
2026-05-13 06:42:49 -0400

    Add ollie run validator

---
+++ oc/compiler/test_suites/ollie_run_validator.c
@@ -0,0 +1,20 @@
+/**
+ *  Author: Jack Robbins
+ *
+ *  The Ollie Run Validator has been designed to, in conjuction with the memory checker,
+ *  give us automated capabilities when validating tests that are able to be validated
+ *  via run. What does it mean to be validated via run? - if we compile and run the program,
+ *  the *shell exit code* should match what we are expecting. Remember that the shell
+ *  exit code is what main returns. Some tests have been designed to do this to validate
+ *  full end-to-end runs
+ */
+
+//We will be doing this multithreaded
+#include <pthread.h>
+
+
+
+int main() {
+
+
+}
