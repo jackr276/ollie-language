a5fdb2dfe00edcb46ed0480d9e35c2fbf9bd5b88
jackr276 <jack_m_robbins@me.com>
2026-05-13 07:04:44 -0400

    Work continued

--- oc/compiler/test_suites/ollie_run_validator.c
+++ oc/compiler/test_suites/ollie_run_validator.c
@@ -8,12 +8,23 @@
  *  exit code is what main returns. Some tests have been designed to do this to validate
  *  full end-to-end runs
  *
+ *
+ *  Strategy is as follows: we first in the main thread read in every single test file in the
+ *  given directory into a big array of all test files. Following that, the array is divided up
+ *  so that every single thread is given it's own subset of test files to work with(index start(inclusive)
+ *  to end(exclusive)). This should avoid the need for us to do any locks. Then each thread 
+ *  is told to start. Internal to the thread, for each file, we will tokenize the file to see if
+ *  we have a special tag TODO WHAT IS THE TAG. If so, we will compile the file using oc, run it
+ *  inside of a subshell, and compare the result with the expected result. If they match we've succeeded,
+ *  if they don't then we have a failure and we put it inside of the error file list
  */
 
 //Use our own in-house dynamic array
 #include "../utils/dynamic_array/dynamic_array.h"
 //We will be doing this multithreaded
 #include <pthread.h>
+#include <sys/types.h>
+#include <time.h>
 
 //We'll need a mutex for all of the files that we wish to operate on
 pthread_mutex_t output_mutex = PTHREAD_MUTEX_INITIALIZER;
@@ -26,11 +37,18 @@ pthread_mutex_t output_mutex = PTHREAD_MUTEX_INITIALIZER;
 dynamic_array_t test_files;
 dynamic_array_t error_files;
 
+//Keep track of the total number of files and the total error files
+u_int32_t number_of_test_files = 0;
+u_int32_t number_of_error_files = 0;
+
+
 
 /**
  * Entry point. This will perform our setup and call into
- * our threads
+ * our threads. We expect that the directory to be used
+ * will be provided as a command line argument along with the
+ * number of threads to use
  */
-int main() {
+int main(int argc, char** argv) {
 
 }
