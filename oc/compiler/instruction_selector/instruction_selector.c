65a6a080271b31a2642cb7484ec0e142aed68e81
jackr276 <jack_m_robbins@me.com>
2026-06-02 06:20:33 -0400

    Begin updating with minus support

--- oc/compiler/instruction_selector/instruction_selector.c
+++ oc/compiler/instruction_selector/instruction_selector.c
@@ -3460,8 +3460,6 @@ static u_int8_t simplify_window(instruction_window_t* window){
 	 * to smash two instructions into one big addressing mode expression, which is a win for us in terms
 	 * of overall complexity and instruction count
 	 */
-
-	//TODO CAN WE UPDATE THIS TO ALL ADDRESSING MODES??
 	if(does_operation_use_addressing_mode(window->instruction2) == TRUE
 		&& window->instruction1->operands.oir.assignee != NULL
 		&& window->instruction1->operands.oir.assignee->variable_type == VARIABLE_TYPE_TEMP
@@ -3676,23 +3674,6 @@ static u_int8_t simplify_window(instruction_window_t* window){
 					 * Anything else is not going to work *if* we're doing an address_operand1
 					 * compression
 					 */
-
-					///
-					///
-					///
-					///
-					///
-					///
-					///
-					///TODO ADD MINUS SUPPORT
-					///
-					///
-					///
-					///
-					///
-					///
-					///
-					///
 					if(to_be_combined->op != PLUS){
 						break;
 					}
@@ -3761,216 +3742,240 @@ static u_int8_t simplify_window(instruction_window_t* window){
 				 */
 				case THREE_ADDR_CODE_BIN_OP_WITH_CONST_STMT:
 					/**
-					 * We are only able to do this *if* we have an addition binary operation.
+					 * We are only able to do this *if* we have an addition/subtraction binary operation.
 					 * Anything else is not going to work *if* we're doing an address_operand1
 					 * compression
 					 */
-					if(to_be_combined->op != PLUS){
-						break;
-					}
+					switch(to_be_combined->op){
+						case PLUS:
+							switch(memory_movement->addressing_mode){
+								/**
+								 * Combine:
+								 * 	t5 <- t6 + 4 
+								 * 	store (t5) <- 8
+								 *
+								 * Into
+								 * store 4(t6) <- 8
+								 */
+								case ADDRESSING_MODE_BASE_ADDRESS_ONLY:
+									//Copy the operands over
+									memory_movement->operands.oir.address_operand1 = to_be_combined->operands.oir.operand1;
+									memory_movement->operands.oir.address_offset = to_be_combined->operands.oir.constant_operand;
 
-					switch(memory_movement->addressing_mode){
-						/**
-						 * Combine:
-						 * 	t5 <- t6 + 4 
-						 * 	store (t5) <- 8
-						 *
-						 * Into
-						 * store 4(t6) <- 8
-						 */
-						case ADDRESSING_MODE_BASE_ADDRESS_ONLY:
-							//Copy the operands over
-							memory_movement->operands.oir.address_operand1 = to_be_combined->operands.oir.operand1;
-							memory_movement->operands.oir.address_offset = to_be_combined->operands.oir.constant_operand;
+									//Update the addressing mode
+									memory_movement->addressing_mode = ADDRESSING_MODE_OFFSET_ONLY;
 
-							//Update the addressing mode
-							memory_movement->addressing_mode = ADDRESSING_MODE_OFFSET_ONLY;
+									//Scrap the old binary operation
+									delete_statement(to_be_combined);
 
-							//Scrap the old binary operation
-							delete_statement(to_be_combined);
+									//Rebuilt around the memory movement
+									reconstruct_window(window, memory_movement);
 
-							//Rebuilt around the memory movement
-							reconstruct_window(window, memory_movement);
+									changed = TRUE;
+									break;
 
-							changed = TRUE;
+								/**
+								 * Combine:
+								 * 	t5 <- t6 + 4 
+								 * 	store 8(t5) <- 8
+								 *
+								 * Into
+								 * store 12(t6) <- 8
+								 */
+								case ADDRESSING_MODE_OFFSET_ONLY:
+									//Add the new constant to the existing offset
+									add_constants(memory_movement->operands.oir.address_offset, to_be_combined->operands.oir.constant_operand);
+
+									//Copy the operand over
+									memory_movement->operands.oir.address_operand1 = to_be_combined->operands.oir.operand1;
+
+									//Scrap the old binary operation
+									delete_statement(to_be_combined);
+
+									//Rebuilt around the memory movement
+									reconstruct_window(window, memory_movement);
+
+									changed = TRUE;
+									break;
+
+								/**
+								 * Combine:
+								 * 	t5 <- t6 + 4 
+								 * 	store 8(t5, t7) <- 8
+								 *
+								 * Into
+								 * store 12(t6, t7) <- 8
+								 */
+								case ADDRESSING_MODE_REGISTERS_AND_OFFSET:
+									//Add the first instruction's constant to the existing offset
+									add_constants(memory_movement->operands.oir.address_offset, to_be_combined->operands.oir.constant_operand);
+
+									//Copy the operand over
+									memory_movement->operands.oir.address_operand1 = to_be_combined->operands.oir.operand1;
+
+									//No mode changes, just delete the old bin op
+									delete_statement(to_be_combined);
+
+									//Rebuild around the memory movement
+									reconstruct_window(window, memory_movement);
+
+									changed = TRUE;
+									break;
+
+								/**
+								 * Combine:
+								 * 	t5 <- t6 + 4 
+								 * 	store x1(t5) <- 8
+								 *
+								 * Into
+								 * store 4+x1(t6) <- 8
+								 */
+								case ADDRESSING_MODE_RIP_RELATIVE:
+									//Copy the offset and operand over
+									memory_movement->operands.oir.address_operand1 = to_be_combined->operands.oir.operand1;
+									memory_movement->operands.oir.address_offset = to_be_combined->operands.oir.constant_operand;
+
+									//This is now RIP-relative with an offset
+									memory_movement->addressing_mode = ADDRESSING_MODE_RIP_RELATIVE_WITH_OFFSET;
+
+									//Scrap the old binary operation
+									delete_statement(to_be_combined);
+
+									//Rebuild around the memory movement
+									reconstruct_window(window, memory_movement);
+
+									changed = TRUE;
+									break;
+
+								/**
+								 * Combine:
+								 * 	t5 <- t6 + 4 
+								 * 	store 8+x1(t5) <- 8
+								 *
+								 * Into
+								 * store 12+x1(t6) <- 8
+								 */
+								case ADDRESSING_MODE_RIP_RELATIVE_WITH_OFFSET:
+									//Add the first instruction's constant to the existing operand
+									add_constants(memory_movement->operands.oir.address_offset, to_be_combined->operands.oir.constant_operand);
+
+									//Copy the operand over
+									memory_movement->operands.oir.address_operand1 = to_be_combined->operands.oir.operand1;
+
+									//Scrap the old binary operation
+									delete_statement(to_be_combined);
+
+									//Rebuild around the memory movement
+									reconstruct_window(window, memory_movement);
+
+									changed = TRUE;
+									break;
+
+								/**
+								 * Combine:
+								 * 	t5 <- t6 + 4 
+								 * 	store 4(t5, t7, 8) <- 8
+								 *
+								 * Into
+								 * 	store 8(t6, t7, 8) <- 8
+								 */
+								case ADDRESSING_MODE_REGISTERS_OFFSET_AND_SCALE:
+									//Add the first instruction's constant to the existing operand
+									add_constants(memory_movement->operands.oir.address_offset, to_be_combined->operands.oir.constant_operand);
+
+									//Copy the operand over
+									memory_movement->operands.oir.address_operand1 = to_be_combined->operands.oir.operand1;
+
+									//Scrap the old binary operation
+									delete_statement(to_be_combined);
+
+									//Rebuild around the memory movement
+									reconstruct_window(window, memory_movement);
+
+									changed = TRUE;
+									break;
+
+								/**
+								 * Combine:
+								 * 	t5 <- t6 + 4 
+								 * 	store (t5, t7) <- 8
+								 *
+								 * Into
+								 * 	store 4(t6, t7) <- 8
+								 */
+								case ADDRESSING_MODE_REGISTERS_ONLY:
+									//First copy the constant over
+									memory_movement->operands.oir.address_offset = to_be_combined->operands.oir.constant_operand;
+
+									//Now copy over the addrss operand
+									memory_movement->operands.oir.address_operand1 = to_be_combined->operands.oir.operand1;
+
+									//This now has an offset so update the mode
+									memory_movement->addressing_mode = ADDRESSING_MODE_REGISTERS_AND_OFFSET;
+
+									//Scrap the old binary operation
+									delete_statement(to_be_combined);
+
+									//Rebuild around the memory movement
+									reconstruct_window(window, memory_movement);
+
+									changed = TRUE;
+									break;
+
+								/**
+								 * Combine:
+								 * 	t5 <- t6 + 4 
+								 * 	store (t5, t7, 8) <- 8
+								 *
+								 * Into
+								 * 	store 4(t6, t7, 8) <- 8
+								 */
+								case ADDRESSING_MODE_REGISTERS_AND_SCALE:
+									//First copy the constant over
+									memory_movement->operands.oir.address_offset = to_be_combined->operands.oir.constant_operand;
+
+									//Now copy over the addrss operand
+									memory_movement->operands.oir.address_operand1 = to_be_combined->operands.oir.operand1;
+
+									//This now has an offset so update the addressing mode
+									memory_movement->addressing_mode = ADDRESSING_MODE_REGISTERS_OFFSET_AND_SCALE;
+
+									//Scrap the old binary operation
+									delete_statement(to_be_combined);
+
+									//Rebuild around the memory movement
+									reconstruct_window(window, memory_movement);
+
+									changed = TRUE;
+									break;
+
+								//Unsupported - just do nothing
+								default:
+									break;
+							}
+							break;
+
+						case MINUS:
+					///
+					///
+					///
+					///
+					///
+					///
+					///TODO ADD MINUS SUPPORT
+					///
+					///
+					///
+					///
+					///
+					///
+					///
+
 							break;
 
 						/**
-						 * Combine:
-						 * 	t5 <- t6 + 4 
-						 * 	store 8(t5) <- 8
-						 *
-						 * Into
-						 * store 12(t6) <- 8
+						 * Something unsupported so we skip it
 						 */
-						case ADDRESSING_MODE_OFFSET_ONLY:
-							//Add the new constant to the existing offset
-							add_constants(memory_movement->operands.oir.address_offset, to_be_combined->operands.oir.constant_operand);
-
-							//Copy the operand over
-							memory_movement->operands.oir.address_operand1 = to_be_combined->operands.oir.operand1;
-
-							//Scrap the old binary operation
-							delete_statement(to_be_combined);
-
-							//Rebuilt around the memory movement
-							reconstruct_window(window, memory_movement);
-
-							changed = TRUE;
-							break;
-
-						/**
-						 * Combine:
-						 * 	t5 <- t6 + 4 
-						 * 	store 8(t5, t7) <- 8
-						 *
-						 * Into
-						 * store 12(t6, t7) <- 8
-						 */
-						case ADDRESSING_MODE_REGISTERS_AND_OFFSET:
-							//Add the first instruction's constant to the existing offset
-							add_constants(memory_movement->operands.oir.address_offset, to_be_combined->operands.oir.constant_operand);
-
-							//Copy the operand over
-							memory_movement->operands.oir.address_operand1 = to_be_combined->operands.oir.operand1;
-
-							//No mode changes, just delete the old bin op
-							delete_statement(to_be_combined);
-
-							//Rebuild around the memory movement
-							reconstruct_window(window, memory_movement);
-
-							changed = TRUE;
-							break;
-
-						/**
-						 * Combine:
-						 * 	t5 <- t6 + 4 
-						 * 	store x1(t5) <- 8
-						 *
-						 * Into
-						 * store 4+x1(t6) <- 8
-						 */
-						case ADDRESSING_MODE_RIP_RELATIVE:
-							//Copy the offset and operand over
-							memory_movement->operands.oir.address_operand1 = to_be_combined->operands.oir.operand1;
-							memory_movement->operands.oir.address_offset = to_be_combined->operands.oir.constant_operand;
-
-							//This is now RIP-relative with an offset
-							memory_movement->addressing_mode = ADDRESSING_MODE_RIP_RELATIVE_WITH_OFFSET;
-
-							//Scrap the old binary operation
-							delete_statement(to_be_combined);
-
-							//Rebuild around the memory movement
-							reconstruct_window(window, memory_movement);
-
-							changed = TRUE;
-							break;
-
-						/**
-						 * Combine:
-						 * 	t5 <- t6 + 4 
-						 * 	store 8+x1(t5) <- 8
-						 *
-						 * Into
-						 * store 12+x1(t6) <- 8
-						 */
-						case ADDRESSING_MODE_RIP_RELATIVE_WITH_OFFSET:
-							//Add the first instruction's constant to the existing operand
-							add_constants(memory_movement->operands.oir.address_offset, to_be_combined->operands.oir.constant_operand);
-
-							//Copy the operand over
-							memory_movement->operands.oir.address_operand1 = to_be_combined->operands.oir.operand1;
-
-							//Scrap the old binary operation
-							delete_statement(to_be_combined);
-
-							//Rebuild around the memory movement
-							reconstruct_window(window, memory_movement);
-
-							changed = TRUE;
-							break;
-
-						/**
-						 * Combine:
-						 * 	t5 <- t6 + 4 
-						 * 	store 4(t5, t7, 8) <- 8
-						 *
-						 * Into
-						 * 	store 8(t6, t7, 8) <- 8
-						 */
-						case ADDRESSING_MODE_REGISTERS_OFFSET_AND_SCALE:
-							//Add the first instruction's constant to the existing operand
-							add_constants(memory_movement->operands.oir.address_offset, to_be_combined->operands.oir.constant_operand);
-
-							//Copy the operand over
-							memory_movement->operands.oir.address_operand1 = to_be_combined->operands.oir.operand1;
-
-							//Scrap the old binary operation
-							delete_statement(to_be_combined);
-
-							//Rebuild around the memory movement
-							reconstruct_window(window, memory_movement);
-
-							changed = TRUE;
-							break;
-
-						/**
-						 * Combine:
-						 * 	t5 <- t6 + 4 
-						 * 	store (t5, t7) <- 8
-						 *
-						 * Into
-						 * 	store 4(t6, t7) <- 8
-						 */
-						case ADDRESSING_MODE_REGISTERS_ONLY:
-							//First copy the constant over
-							memory_movement->operands.oir.address_offset = to_be_combined->operands.oir.constant_operand;
-
-							//Now copy over the addrss operand
-							memory_movement->operands.oir.address_operand1 = to_be_combined->operands.oir.operand1;
-
-							//This now has an offset so update the mode
-							memory_movement->addressing_mode = ADDRESSING_MODE_REGISTERS_AND_OFFSET;
-
-							//Scrap the old binary operation
-							delete_statement(to_be_combined);
-
-							//Rebuild around the memory movement
-							reconstruct_window(window, memory_movement);
-
-							changed = TRUE;
-							break;
-
-						/**
-						 * Combine:
-						 * 	t5 <- t6 + 4 
-						 * 	store (t5, t7, 8) <- 8
-						 *
-						 * Into
-						 * 	store 4(t6, t7, 8) <- 8
-						 */
-						case ADDRESSING_MODE_REGISTERS_AND_SCALE:
-							//First copy the constant over
-							memory_movement->operands.oir.address_offset = to_be_combined->operands.oir.constant_operand;
-
-							//Now copy over the addrss operand
-							memory_movement->operands.oir.address_operand1 = to_be_combined->operands.oir.operand1;
-
-							//This now has an offset so update the addressing mode
-							memory_movement->addressing_mode = ADDRESSING_MODE_REGISTERS_OFFSET_AND_SCALE;
-
-							//Scrap the old binary operation
-							delete_statement(to_be_combined);
-
-							//Rebuild around the memory movement
-							reconstruct_window(window, memory_movement);
-
-							changed = TRUE;
-							break;
-
-						//Unsupported - just do nothing
 						default:
 							break;
 					}
