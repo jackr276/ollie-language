eb6c3239407ee95127cc65eca635aa648cafee79
jackr276 <jack_m_robbins@me.com>
2026-06-07 09:14:47 -0400

    Add new graph analyzer module

---
+++ oc/compiler/graph_analyzer/graph_analyzer.c
@@ -0,0 +1,7 @@
+/**
+ * Author: Jack Robbins
+ * This C file contains the implementations for APIs defined inside of the header file
+ * of the same name
+ */
+
+#include "graph_analyzer.h"
---
+++ oc/compiler/graph_analyzer/graph_analyzer.h
@@ -0,0 +1,16 @@
+/**
+ * Author: Jack Robbins
+ * This module contains everything needed to compute CFG dominance relations in
+ * Ollie.
+ */
+
+//Include guards
+#ifndef GRAPH_ANALYZER_H
+#define GRAPH_ANALYZER_H
+
+//Link to the CFG
+#include "../cfg/cfg.h"
+
+
+
+#endif /* GRAPH_ANALYZER_H */
