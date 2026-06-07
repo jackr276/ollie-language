aacbfa16267aba051697d6072b7d8f08491b12a5
jackr276 <jack_m_robbins@me.com>
2026-06-07 11:43:44 -0400

    Dominator set calculation added

--- oc/compiler/graph_analyzer/graph_analyzer.c
+++ oc/compiler/graph_analyzer/graph_analyzer.c
@@ -136,11 +136,52 @@ dynamic_array_t compute_reverse_post_order_traversal(basic_block_t* entry){
 
 
 /**
+ * A recursive post order simplifies the code, so it's what we'll use here
+ */
+static void post_order_traversal_rec(dynamic_array_t* post_order_traversal, basic_block_t* entry){
+	//If we've visited this one before, skip
+	if(entry->visited == TRUE){
+		return;
+	}
+
+	//Otherwise mark that we've visited
+	entry->visited = TRUE;
+
+	//Run through every successor
+	for(u_int32_t i = 0; i < entry->successors.current_index; i++){
+		//Recursive call to every child first
+		post_order_traversal_rec(post_order_traversal, dynamic_array_get_at(&(entry->successors), i));
+	}
+	
+	//Now we'll finally visit the node
+	dynamic_array_add(post_order_traversal, entry);
+}
+
+
+/**
+ * Get and return the regular postorder traversal for a function-level CFG
+ *
+ * NOTE: This assumes that the caller has already wiped the function's visited
+ * status clean
+ */
+static dynamic_array_t compute_post_order_traversal(basic_block_t* entry){
+	//Create our dynamic array
+	dynamic_array_t post_order_traversal = dynamic_array_alloc();
+
+	//Make the recursive call
+	post_order_traversal_rec(&post_order_traversal, entry);
+
+	//Give the traversal back
+	return post_order_traversal;
+}
+
+
+/**
  * Calculate all reverse traversals for a given function. A reverse traversal is simply a traversal on the graph
  * where every successor is a predecessor, and every predecessor is a successor. This is needed for the postdominance
  * computation
  */
-static void calculate_all_reverse_traversals(basic_block_t* function_entry_block, dynamic_array_t* function_blocks){
+static inline void calculate_all_reverse_traversals(basic_block_t* function_entry_block, dynamic_array_t* function_blocks){
 	//Set the RPO to be null
 	if(function_entry_block->reverse_post_order.internal_array != NULL){
 		dynamic_array_dealloc(&(function_entry_block->reverse_post_order));
@@ -166,6 +207,142 @@ static void calculate_all_reverse_traversals(basic_block_t* function_entry_block
 
 
 /**
+ * Calculate the dominator sets for each and every node
+ *
+ * For each node in the nodeset:
+ * 	dom(N) <- All nodes
+ *	
+ * Worklist = {StartNode}
+ * while worklist is not empty
+ * 	Remove any node Y from Worklist
+ * 	New = {Y} U {X | X elem Pred(Y)}
+ *
+ * 	if new != dom 
+ * 		Dom(Y) = New
+ * 		For each successor X of Y
+ * 			add X to the worklist
+ *
+ * This algorithm repeats indefinitely UNTIL a stable solution
+ * is found(this is when new == DOM for every node, hence there's nowhere
+ * left to go)
+ *
+ * NOTE: We repeat this for each and every function in the CFG. If blocks aren't in
+ * the same function, then their dominance is completely unrelated
+ */
+static void calculate_dominator_sets(basic_block_t* function_entry_block, dynamic_array_t* function_blocks){
+	/**
+	 * Every node in the CFG has a dominator set that is set
+	 * to be identical to the list of all nodes
+	 */
+	for(u_int32_t i = 0; i < function_blocks->current_index; i++){
+		basic_block_t* block = dynamic_array_get_at(function_blocks, i);
+
+		/**
+		 * We will initialize the block's dominator set to be the entire set of nodes
+		 * in the given function
+		 */
+		block->dominator_set = clone_dynamic_array(function_blocks);
+	}
+
+	//Initialize a "worklist" dynamic array for this particular function
+	dynamic_array_t worklist = dynamic_array_alloc();
+
+	//Add this into the worklist as a seed
+	dynamic_array_add(&worklist, function_entry_block);
+	
+	//The new dominance frontier that we have each time
+	dynamic_array_t new;
+
+	//So long as the worklist is not empty
+	while(dynamic_array_is_empty(&worklist) == FALSE){
+		//Remove a node Y from the worklist(remove from back - most efficient{O(1)})
+		basic_block_t* Y = dynamic_array_delete_from_back(&worklist);
+		
+		//Create the new dynamic array that will be used for the next dominator set
+		new = dynamic_array_alloc();
+
+		//We will add Y into it's own dominator set
+		dynamic_array_add(&new, Y);
+
+		//If Y has predecessors, we will find the intersection of their dominator sets
+		if(Y->predecessors.internal_array != NULL){
+			//Grab the very first predecessor's dominator set
+			dynamic_array_t pred_dom_set = ((basic_block_t*)(Y->predecessors.internal_array[0]))->dominator_set;
+
+			//Are we in the intersection of the dominator sets?
+			u_int8_t in_intersection;
+
+			//We will now search every item in this dominator set
+			for(u_int32_t i = 0; i < pred_dom_set.current_index; i++){
+				//Grab the dominator out
+				basic_block_t* dominator = dynamic_array_get_at(&pred_dom_set, i);
+
+				/**
+				 * By default we assume that this given dominator is in the set. If it
+				 * isn't we'll set it appropriately
+				 */
+				in_intersection = TRUE;
+
+				/**
+				 * An item is in the intersection if and only if it is contained 
+				 * in all of the dominator sets of the predecessors of Y
+				 *
+				 * We'll start at 1 here - we've already accounted for 0
+				*/
+				for(u_int32_t j = 1; j < Y->predecessors.current_index; j++){
+					//Grab our other predecessor
+					basic_block_t* other_predecessor = Y->predecessors.internal_array[j];
+
+					/**
+					 * Now we will go over this predecessor's dominator set, and see if "dominator"
+					 * is also contained within it
+					 */
+
+					//Let's check for it in here. If we can't find it, we set the flag to false and bail out
+					if(dynamic_array_contains(&(other_predecessor->dominator_set), dominator) == NOT_FOUND){
+						in_intersection = FALSE;
+						break;
+					}
+				
+					/**
+					 * Otherwise we did find it, so we'll look at the next predecessor, and see if it is also
+					 * in there. If we get to the end and "in_intersection" is true, then we know that we've
+					 * found this one dominator in every single set
+					 */
+				}
+
+				if(in_intersection == TRUE){
+					//Add the dominator in
+					dynamic_array_add(&new, dominator);
+				}
+			}
+		}
+
+		//Now we'll check - are these two dominator sets the same? If not, we'll need to update them
+		if(dynamic_arrays_equal(&new, &(Y->dominator_set)) == FALSE){
+			//Destroy the old one
+			dynamic_array_dealloc(&(Y->dominator_set));
+
+			//And replace it with the new
+			Y->dominator_set = new;
+
+			//Now for every successor of Y, add it into the worklist
+			for(u_int32_t i = 0; i < Y->successors.current_index; i++){
+				dynamic_array_add(&worklist, Y->successors.internal_array[i]);
+			}
+
+		//Otherwise they are the same, so destroy the one that we just made
+		} else {
+			dynamic_array_dealloc(&new);
+		}
+	}
+
+	//Destroy the worklist now that we're done with it
+	dynamic_array_dealloc(&worklist);
+}
+
+
+/**
  * We will calculate:
  *  1.) Reverse post order traversals
  *  2.) Dominator Sets
@@ -181,8 +358,7 @@ void calculate_all_control_flow_relations_for_function(basic_block_t* function_e
 	//Before any calculation can be done, we need to compute every single reverse traversal
 	calculate_all_reverse_traversals(function_entry_block, function_blocks);
 	
-	//We first need to calculate the dominator sets of every single node
-	//TODO DOC
+	//Now calculate the dominator set for every function block
 	calculate_dominator_sets(function_entry_block, function_blocks);
 
 	//Now we'll build the dominator tree up
