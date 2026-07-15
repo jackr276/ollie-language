5ddb1f40411c686c7f738d068f3f385d97377e09
jackr276 <jack_m_robbins@me.com>
2026-07-15 07:03:28 -0400

    Now fully tied into build system

---
+++ oc/compiler/dependency_graph/dependency_graph.c
@@ -0,0 +1,6 @@
+/**
+ * Author: Jack Robbins
+ * This C file contains the definitions for the APIs defined in the header file of the same name
+ */
+
+#include "dependency_graph.h"
---
+++ oc/compiler/dependency_graph/dependency_graph.h
@@ -0,0 +1,12 @@
+/**
+ * Author: Jack Robbins
+ * This header file exposes the needed APIs for other files to use the dependency graph
+ */
+
+#ifndef DEPENDENCY_GRAPH_H
+#define DEPENDENCY_GRAPH_H
+
+
+
+
+#endif /* DEPENDENCY_GRAPH_H */
