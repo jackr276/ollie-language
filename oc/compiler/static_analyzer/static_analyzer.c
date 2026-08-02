a580b764a3de8ff79eeda9acad3e4669babd6db2
jackr276 <jack_m_robbins@me.com>
2026-08-02 11:05:07 -0400

    Add static analyzer module and tie into build system

---
+++ oc/compiler/static_analyzer/static_analyzer.c
@@ -0,0 +1,5 @@
+/**
+ * Author: Jack Robbins
+ * This file implements the APIs for the static analyzer that were defined in the
+ * header file of the same name
+ */
---
+++ oc/compiler/static_analyzer/static_analyzer.h
@@ -0,0 +1,12 @@
+/**
+ * Author: Jack Robbins
+ * This header file defines the API for the static analyzer. The static analyzer will
+ * convert the CFG into SSA form, perform uninitialized variable detection, mutation
+ * checking, and other static analysis functions
+ */
+
+
+#ifndef STATIC_ANALYZER_H
+#define STATIC_ANALYZER_H
+
+#endif /* STATIC_ANALYZER_H */
