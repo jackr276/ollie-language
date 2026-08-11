c3bc7e194ed82aa3d05f52815b88b75eacefb3f2
jackr276 <jack_m_robbins@me.com>
2026-08-11 16:57:45 -0400

    Last two tests fixed to use the unified option parser

--- oc/compiler/test_suites/instruction_selector_test.c
+++ oc/compiler/test_suites/instruction_selector_test.c
@@ -18,70 +18,7 @@
 #include "../optimizer/optimizer.h"
 #include "../instruction_selector/instruction_selector.h"
 #include "../utils/constants.h"
-
-u_int32_t num_warnings;
-u_int32_t num_errors;
-
-
-/**
- * We'll use this helper function to process the compiler flags and return a structure that
- * tells us what we need to do throughout the compiler
- */
-static compiler_options_t* parse_and_store_options(int argc, char** argv){
-	//Allocate it
-	compiler_options_t* options = calloc(1, sizeof(compiler_options_t));
-	
-	//For storing our opt
-	int opt;
-
-	//Run through all of our options
-	while((opt = getopt(argc, argv, "iatdhsf:o:?")) != -1){
-		//Switch based on opt
-		switch(opt){
-			//Invalid option
-			case '?':
-				printf("Invalid option: %c\n", optopt);
-				exit(0);
-			//Specify that we want to print intermediate representations
-			case 'i':
-				options->print_irs = TRUE;
-				break;
-			//After we print help we exit
-			case 'h':
-				exit(0);
-			//Time execution for performance test
-			case 't':
-				options->time_execution = TRUE;
-				break;
-			//Store the input file name
-			case 'f':
-				options->file_name = optarg;
-				break;
-			//Turn on debug printing
-			case 'd':
-				options->enable_debug_printing = TRUE;
-				break;
-			//Specify that we want a summary to be shown
-			case 's':
-				options->show_summary = TRUE;
-				break;
-			//Specific output file
-			case 'o':
-				options->output_file = optarg;
-				break;
-		}
-	}
-
-	//This is an error, so we'll fail out here
-	if(options->file_name == NULL){
-		printf("[COMPILER ERROR]: No input file name provided. Use -f <filename> to specify a .ol source file\n");
-		exit(1);
-	}
-
-	//Give back the options we got in the structure
-	return options;
-}
-
+#include "../utils/option_parser/option_parser.h"
 
 /**
  * Our main and only function
@@ -90,13 +27,13 @@ int main(int argc, char** argv){
 	//How much time we've spent
 	double time_spent;
 	//Initialize these both to 0
-	num_errors = 0;
-	num_warnings = 0;
+	u_int32_t num_errors = 0;
+	u_int32_t num_warnings = 0;
 
 	printf("==================================== INSTRUCTION SELECTOR TEST ======================================\n");
 
 	//Parse and store the options
-	compiler_options_t* options = parse_and_store_options(argc, argv);
+	compiler_options_t* options = parse_and_store_options(argc, argv, &num_warnings, &num_errors);
 
 	//Do we want to time execution or not
 	u_int8_t time_execution = options->time_execution;
