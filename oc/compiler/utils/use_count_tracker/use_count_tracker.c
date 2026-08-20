fa7ad75faf58103cd551cf5129b6d0f4c8cde421
jackr276 <jack_m_robbins@me.com>
2026-08-20 19:23:49 -0400

    Tie the use count tracker into the build system

--- oc/compiler/utils/use_count_tracker/use_count_tracker.c
+++ oc/compiler/utils/use_count_tracker/use_count_tracker.c
@@ -2,6 +2,9 @@
  * Author: Jack Robbins
  * This file implements the APIs defined in the header file of the same name for the
  * variable use count tracker
+ *
+ * The use count tracker only comes into play inside of the instruction selector. It will
+ * only be included in builds that require the instruction selector
  */
 
 #include "use_count_tracker.h"
