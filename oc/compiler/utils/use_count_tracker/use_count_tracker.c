4f5da360bb9798f3f6fc1ffb26774156e375cb43
jackr276 <jack_m_robbins@me.com>
2026-08-21 06:50:11 -0400

    Implement the unique dynamic resize

--- oc/compiler/utils/use_count_tracker/use_count_tracker.c
+++ oc/compiler/utils/use_count_tracker/use_count_tracker.c
@@ -9,6 +9,8 @@
 
 #include "use_count_tracker.h"
 #include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
 
 /**
  * Allocate the underlying data structures in the use count tracker
@@ -28,17 +30,45 @@ use_count_tracker_t use_count_tracker_alloc(u_int32_t initial_variable_count){
 	return tracker;
 }
 
+
+/**
+ * Perform a dynamic resize on the use count tracker based on the ID that was requested. To
+ * be safe, we will always reallocate with double what was requested
+ */
+static inline void perform_dynamic_resize(use_count_tracker_t* tracker, u_int32_t requested_id){
+	//Nothing to worry about here
+	if(tracker->variable_count < requested_id){
+		return;
+	}
+
+	//Cache the old variable count and reup it
+	u_int32_t old_variable_count = tracker->variable_count;
+	tracker->variable_count = requested_id * 2;
+
+	//Reallocate our map with the new count
+	tracker->map = realloc(tracker->map, sizeof(u_int16_t) * tracker->variable_count);
+
+	/**
+	 * We need to make sure that this is all set to 0's. So, grab a pointer to the new 
+	 * region by skipping over all of the variables that already exist, and set all of
+	 * that memory to be 0's
+	 */
+	memset(tracker->map + old_variable_count, 0, sizeof(u_int16_t) * tracker->variable_count - old_variable_count);
+}
+
+
 /**
  * Retrieve the use count for a given ID
  */
-inline u_int32_t get_use_count_by_id(u_int32_t id){
+inline u_int32_t get_use_count_by_id(use_count_tracker_t* tracker, u_int32_t id){
 
 }
+
 
 /**
  * Increment the use count for a given ID
  */
-inline void increment_use_count(u_int32_t id){
+inline void increment_use_count(use_count_tracker_t* tracker, u_int32_t id){
 
 }
 
