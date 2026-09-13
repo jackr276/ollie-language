634b0c62f9052ee8830a4347bba83049d9ba5828
jackr276 <jack_m_robbins@me.com>
2026-09-13 15:24:32 -0400

    Error list parsing added

--- oc/compiler/parser/parser.c
+++ oc/compiler/parser/parser.c
@@ -14328,8 +14328,6 @@ static inline u_int8_t parse_function_parameters(ollie_token_stream_t* token_str
  * error
  *
  * <error-list> = (<error> {, <error>}*)
- *
- * TODO FINISH REWORK
  */
 static inline u_int8_t error_list2(ollie_token_stream_t* token_stream, generic_type_t* function_type){
 	//Extract the internal function type
@@ -14347,122 +14345,64 @@ static inline u_int8_t error_list2(ollie_token_stream_t* token_stream, generic_t
 	//Push onto the grouping stack
 	push_token(&grouping_stack, lookahead);
 
-	//Start the error count off at 0
-	int32_t error_count = 0;
-
-	//Now we need to see at least one, but possibly many, error types in here
-	do {
-		//Get the next token
+	/**
+	 * Run through until we stop seeing errors. We'll know when
+	 * to stop once we don't see a comma anymore
+	 */
+	while(TRUE){
+		//Refresh the token - we should see an identifier
 		lookahead = get_next_token(token_stream, &parser_line_num);
-
-		//If we don't see an ident then this is a failure
 		if(lookahead.tok != IDENT){
 			sprintf(info, "Expected to see a custom error type, but instead say \"%s\"", lexitem_to_string(&lookahead));
-			print_parse_message(MESSAGE_TYPE_ERROR, info, parser_line_num);
-			num_errors++;
-			return FAILURE;
+			return print_and_return_failure(info, parser_line_num);
 		}
 
-		//If we make it here we're on the right track, let's see what we can find. Remember that all
-		//types are defacto immutalbe
+		//This should be a type - if it's not then fail out
 		symtab_type_record_t* found_type = lookup_type_name_only(type_symtab, lookahead.lexeme.string, NOT_MUTABLE);
-
-		//We can't find it - big problem
 		if(found_type == NULL){
 			sprintf(info, "There exists no error type with the name \"%s\"", lookahead.lexeme.string);
-			print_parse_message(MESSAGE_TYPE_ERROR, info, parser_line_num);
-			num_errors++;
-			return FAILURE;
+			return print_and_return_failure(info, parser_line_num);
 		}
 
-		//Get the inner type out
+		//Otherwise we did find it - but is it an ERROR? Remember we are only allowed to raise error types
 		generic_type_t* error_type = found_type->type;
-
-		//Make sure that we dealias this - it is possible to alias any type
-		error_type = dealias_type(error_type);
-
-		//Otherwise we did find it - but is it an ERROR? Remember we are only allowed to raise error types, not just any
-		//old type
 		if(error_type->type_class != TYPE_CLASS_ERROR){
 			sprintf(info, "Type \"%s\" is not an error type and cannot be raised by a function as one", lookahead.lexeme.string);
-			print_parse_message(MESSAGE_TYPE_ERROR, info, parser_line_num);
-			num_errors++;
-			return FAILURE;
+			return print_and_return_failure(info, parser_line_num);
 		}
 
 		/**
-		 * If we're not defining something that was predeclared, then all we need to do
-		 * is add this in
+		 * We cannot put the same error in this list twice, so before adding scan through
+		 * and verify that these are unique
 		 */
-		if(defining_predeclared_function == FALSE){
-			//Let's first check for duplicated errors
-			for(int32_t i = 0; i < internal_function_type->potential_errors.current_index; i++){
-				//Extrace it
-				generic_type_t* candidate = dynamic_array_get_at(&(internal_function_type->potential_errors), i);
+		for(int32_t i = 0; i < internal_function_type->potential_errors.current_index; i++){
+			generic_type_t* candidate = dynamic_array_get_at(&(internal_function_type->potential_errors), i);
 
-				//If they're equal at all, we fail out
-				if(types_identical(candidate, error_type) == TRUE){
-					sprintf(info, "Function is already declared as raising an error of \"%s\"" , error_type->type_name.string);
-					print_parse_message(MESSAGE_TYPE_ERROR, info, parser_line_num);
-					num_errors++;
-					return FAILURE;
-				}
-			}
-
-			//Add it in
-			dynamic_array_add(&(internal_function_type->potential_errors), error_type);
-
-		} else {
-			//We have too many - we need to bail out
-			if(error_count >= internal_function_type->potential_errors.current_index){
-				sprintf(info, "Function was predeclared as only having %d errors", internal_function_type->potential_errors.current_index); 
-				print_parse_message(MESSAGE_TYPE_ERROR, info, parser_line_num);
-				num_errors++;
-				return FAILURE;
-			}
-
-			//Extract the predeclared version
-			generic_type_t* predeclared_error = dynamic_array_get_at(&(internal_function_type->potential_errors), error_count);
-
-			//If this isn't an exact match, we fail out
-			if(predeclared_error != error_type){
-				sprintf(info, "Function was predeclared with error number %d as \"%s\", but declared with \"%s\" as error number %d", error_count + 1, predeclared_error->type_name.string, error_type->type_name.string, error_count + 1);
-				print_parse_message(MESSAGE_TYPE_ERROR, info, parser_line_num);
-				num_errors++;
-				return FAILURE;
+			//If they're equal at all, we fail out
+			if(types_identical(candidate, error_type) == TRUE){
+				sprintf(info, "Function is already declared as raising an error of \"%s\"" , error_type->type_name.string);
+				return print_and_return_failure(info, parser_line_num);
 			}
 		}
 
-		//Bump the error count up
-		error_count++;
+		//All fine to add it in now
+		dynamic_array_add(&(internal_function_type->potential_errors), error_type);
 
 		//Now we can either see a comma or the closing paren
 		lookahead = get_next_token(token_stream, &parser_line_num);
-
-		//If we have a comma then continue
 		if(lookahead.tok == COMMA){
 			continue;
-
-		//If we have an R_PAREN then get out
 		} else if(lookahead.tok == R_PAREN){
 			break;
-
-		//Otherwise this is an error
 		} else {
 			sprintf(info, "Expected , or ) but got \"%s\"", lexitem_to_string(&lookahead));
-			print_parse_message(MESSAGE_TYPE_ERROR, info, parser_line_num);
-			num_errors++;
-			return FAILURE;
+			return print_and_return_failure(info, parser_line_num);
 		}
-
-	//Loop forever until one of our exit cases is hit
-	} while(TRUE);
+	}
 
 	//We can only ever get here if we saw the R_PAREN. Make sure we can match it
 	if(pop_token(&grouping_stack).tok != L_PAREN){
-		print_parse_message(MESSAGE_TYPE_ERROR, "Unmatched parenthesis detected", parser_line_num);
-		num_errors++;
-		return FAILURE;
+		return print_and_return_failure("Unmatched parenthesis detected", parser_line_num);
 	}
 
 	//With that we are done, we can return success
@@ -14518,10 +14458,7 @@ static inline u_int8_t parse_function_return_type_and_error_list(ollie_token_str
 		clear_dynamic_set(&errors_raised_by_current_function);
 
 		//Now that we've made it past that, we can let the helper do the parsing for us
-		u_int8_t success = error_list(token_stream, function_signature);
-
-		//Fail out if bad
-		if(success == FAILURE){
+		if(error_list2(token_stream, function_signature) == FAILURE){
 			return print_and_return_failure("Invalid error list detected in function declaration", parser_line_num);
 		}
 
@@ -14683,12 +14620,16 @@ static generic_ast_node_t* function_definition2(ollie_token_stream_t* token_stre
 		return ast_node_alloc(AST_NODE_TYPE_ERR_NODE, SIDE_TYPE_LEFT);
 	}
 
+
 	/**
-	 * We should now be able to get the return type out and add that to the signature
-	 * as well
+	 * We should now be able to get the return type and any error
+	 * raising types out and add that to the signature as well
 	 *
 	 * TODO WE NEED THE RETURN BY COPY REMEDIATION AFTER WE CREATE THE RECORD
 	 */
+	if(parse_function_return_type_and_error_list(token_stream, new_function_signature) == FALSE){
+		return ast_node_alloc(AST_NODE_TYPE_ERR_NODE, SIDE_TYPE_LEFT);
+	}
 
 
 
