13807b5bc5acf54b5f8b3d31c9f4476adbce4f55
jackr276 <jack_m_robbins@me.com>
2026-09-23 05:55:38 -0400

    Continue work on the initializers

--- oc/compiler/utils/three_address_initializer/three_address_initializer.h
+++ oc/compiler/utils/three_address_initializer/three_address_initializer.h
@@ -15,17 +15,43 @@
 //These will contain constants and variables
 #include "../three_address_constant.h"
 #include "../three_address_variable.h"
+#include <sys/types.h>
+
+//Forward declarations
+typedef struct three_addr_initializer_t three_addr_initializer_t;
+typedef struct initializer_result_t initializer_result_t;
+
 
 /**
  * A three address initializer may contain constants, variables *OR*
  * nested initializers inside of it
  */
-typedef struct three_addr_initializer_t three_addr_initializer_t;
 struct three_addr_initializer_t {
-
-	//TODO NEED DYNAMIC RESIZE
-
-	//TODO
+	//Unique identifier
+	u_int32_t initializer_id;
+	//Store the type as well
+	generic_type_t* type;
+	/**
+	 * The "Initializer results" mimics dynamic array functionality
+	 * almost to a T, except that instead of storing pointers, we are
+	 * instead store initializer_result_t objects which are themselves like
+	 * tagged unions of values
+	 */
+	initializer_result_t* initializer_results;
+	int32_t results_max_index;
+	int32_t results_current_index;
 };
+
+
+/**
+ * Dynamically allocate an initializer of a given type
+ */
+three_addr_initializer_t* three_addr_initializer_alloc(generic_type_t* type);
+
+
+/**
+ * Destroy a given initializer
+ */
+void three_addr_initializer_dealloc(generic_type_t* type);
 
 #endif /* THREE_ADDRESS_INITIALIZER_H */
