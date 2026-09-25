bf2e09b3458ceb2091c129b0c06b48d8f6a3d023
jackr276 <jack_m_robbins@me.com>
2026-09-25 07:14:39 -0400

    Add a call to the dynamic resize before we populate things

--- oc/compiler/instruction_selector/instruction_selector.c
+++ oc/compiler/instruction_selector/instruction_selector.c
@@ -1821,6 +1821,11 @@ static inline void perform_call_lowering_in_function(symtab_function_record_t* f
 static inline void populate_use_counts_for_function(dynamic_array_t* function_blocks){
 	for(int32_t i = 0; i < function_blocks->current_index; i++){
 		basic_block_t* block = dynamic_array_get_at(function_blocks, i);
+
+		/**
+		 * We may as well do this now to resize if there are any changes in the number of variables.
+		 */
+		use_count_tracker_dynamic_resize_for_id(&use_count_tracker, get_current_variable_id());
 		
 		//Run through every signle instruction
 		instruction_t* instruction_cursor = block->leader_statement;
